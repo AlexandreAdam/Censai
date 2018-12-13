fixed lens+source params except ellipticity angle, learning averaged labels
Former-commit-id: 0c39df165c2010f68058efa54b966359d934ce9b

Censai/get_data.py
@@ -135,15 +135,15 @@ class DataGenerator(object):
                     #parameters for kappa
                     xlens = 0
                     ylens = 0
-                    elp = np.random.uniform()
+                    elp = 0.4#np.random.uniform()
                     phi = np.random.uniform(low=0.0, high=2.*np.pi)
-                    Rein = np.random.uniform(low=0.5, high = 3.)
+                    Rein = 1.2 #np.random.uniform(low=0.5, high = 3.)
 
                     #parameters for source
-                    sigma_src = np.random.uniform(low=0, high=0.5)
+                    sigma_src = 0.1 # np.random.uniform(low=0, high=0.5)
                     #np.random.normal(loc=0.0, scale = 0.01)
-                    x_src = np.random.uniform(low=-0.16, high=0.16)
-                    y_src =  np.random.uniform(low=-0.16, high=0.16)
+                    x_src = 0. #np.random.uniform(low=-0.16, high=0.16)
+                    y_src =  0. #np.random.uniform(low=-0.16, high=0.16)
 
                     source[i,:,:,0] = self.gen_source(Xsrc, Ysrc, x_src = x_src, y_src = y_src, sigma_src = sigma_src, numpix_side = self.numpix_side, norm = norm_source)
 
@@ -162,15 +162,15 @@ class DataGenerator(object):
                     #parameters for kappa
                     xlens = 0
                     ylens = 0
-                    elp = np.random.uniform()
+                    elp = 0.4#np.random.uniform()
                     phi = np.random.uniform(low=0.0, high=2.*np.pi)
-                    Rein = np.random.uniform(low=0.5, high = 3.)
+                    Rein = 1.2 #np.random.uniform(low=0.5, high = 3.)
 
                     #parameters for source
-                    sigma_src = np.random.uniform(low=0, high=0.5)
+                    sigma_src = 0.1 # np.random.uniform(low=0, high=0.5)
                     #np.random.normal(loc=0.0, scale = 0.01)
-                    x_src = np.random.uniform(low=-0.16, high=0.16)
-                    y_src =  np.random.uniform(low=-0.16, high=0.16)
+                    x_src = 0. #np.random.uniform(low=-0.16, high=0.16)
+                    y_src =  0. #np.random.uniform(low=-0.16, high=0.16)
 
                     
                     self.source[i,:,:,0] = self.gen_source(Xsrc, Ysrc, x_src = x_src, y_src = y_src, sigma_src = sigma_src, numpix_side = self.numpix_side, norm = norm_source)

test_hires.py
@@ -54,7 +54,7 @@ tf.app.flags.DEFINE_integer('depth', 1,
                             """Depth of the network""")
 tf.app.flags.DEFINE_integer('batch_size', 5,
                             """Number of samples per batch.""")
-tf.app.flags.DEFINE_integer('t_max', 4,
+tf.app.flags.DEFINE_integer('t_max', 10,
                             """The number of time steps to train on. """
                             """If -1 it will be drawn randomly from a geometrix distribution.""")
 tf.app.flags.DEFINE_integer('j_min', 6,
@@ -93,7 +93,7 @@ def get_psnr(x_est, x_true):
 def train():
 
     # This is the file that we will save the model to.
-    model_name = os.environ['CENSAI_PATH']+ '/trained_weights/RIM_kappa/Censai_hires_gradclip1000.ckpt'
+    model_name = os.environ['CENSAI_PATH']+ '/trained_weights/RIM_kappa/Censai_hires_findangle.ckpt'
 
     
     # DEFINE LAURENCE's stuff
@@ -279,7 +279,7 @@ def train():
         Datagen.kappatest = np.zeros((test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side,n_channel  )) 
         
         Datagen.sourcetest, Datagen.kappatest = Datagen.read_data_batch(Datagen.Xtest, Datagen.sourcetest, Datagen.kappatest, 'test', 'gen')
-        imgs = np.zeros((5,test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side, n_channel ))
+        imgs = np.zeros((11,test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side, n_channel ))
         true_data = np.zeros((test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side, n_channel ))
         pred_lens_image = np.zeros((test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side, n_channel ))
         last_grad = np.zeros((test_batch_size, Datagen.numkappa_side , Datagen.numkappa_side, n_channel ))
@@ -316,12 +316,12 @@ def train():
                 valid_psnr /= 10.
                 
                 print 'saving...'
-                np.save('last_grad.npy', last_grad)
-                np.save('pred_lens_imageT8.npy', pred_lens_image)
-                np.save('true_dataT8.npy', true_data)
-                np.save('source_imageT8.npy', Datagen.sourcetest )
-                np.save('kappa_recT8.npy', imgs)
-                np.save('kappa_mapT8.npy', Datagen.kappatest)
+                np.save('last_grad_fangle.npy', last_grad)
+                np.save('pred_lens_image_fangle.npy', pred_lens_image)
+                np.save('true_data_fangle.npy', true_data)
+                np.save('source_image_fangle.npy', Datagen.sourcetest )
+                np.save('kappa_rec_fangle.npy', imgs)
+                np.save('kappa_map_fangle.npy', Datagen.kappatest)
   
 #
 #                    # Display logs per epoch step