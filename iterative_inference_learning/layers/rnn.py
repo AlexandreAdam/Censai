055b400dbb6097027f51b2ae33a206219e74c7df
Yashar Hezaveh <yashar.hez@gmail.com>
2018-12-18 16:56:07 -0500

    making a double RIM in a crazt way

--- iterative_inference_learning/layers/rnn.py
+++ iterative_inference_learning/layers/rnn.py
@@ -39,16 +39,19 @@ def _state_size_with_prefix(state_size, prefix=None):
 # END WARREN HACK
 
 
-def flex_rnn(cell, loop_fn, init_fn=None, dtype=tf.float32,
+def flex_rnn(cell1, cell2, loop_fn1, loop_fn2, init_fn1=None, init_fn2=None, dtype=tf.float32,
             parallel_iterations=10, swap_memory=False, scope=None):
 
-  if not isinstance(cell, rnn_cell.RNNCell):
+  if not isinstance(cell1, rnn_cell.RNNCell):
     raise TypeError("cell must be an instance of RNNCell")
-  if not callable(loop_fn):
+  if not callable(loop_fn1):
     raise TypeError("loop_fn must be a callable")
 
-  if init_fn is None:
-    init_fn = loop_fn
+  if init_fn1 is None:
+    init_fn1 = loop_fn1
+
+  if init_fn2 is None:
+    init_fn2 = loop_fn2
 
   # Create a new scope in which the caching device is either
   # determined by the parent scope, or is set to place the cached
@@ -59,29 +62,39 @@ def flex_rnn(cell, loop_fn, init_fn=None, dtype=tf.float32,
 
     # Perform first time step
     time = constant_op.constant(0, dtype=dtypes.int32)
-    (elements_finished, new_input, current_output, current_state) = init_fn(time, cell)
+    (elements_finished1, new_input1, current_output1, current_state1) = init_fn1(time, cell1)
+    (elements_finished2, new_input2, current_output2, current_state2) = init_fn2(time, cell2)
 
     # Gather some information about the RNN Output
-    flat_output_structure = nest.flatten(current_output)
-    flat_output_dtypes = [emit.dtype for emit in flat_output_structure]
+    flat_output_structure1 = nest.flatten(current_output1)
+    flat_output_dtypes1 = [emit.dtype for emit in flat_output_structure1]
+    flat_output_structure2 = nest.flatten(current_output2)
+    flat_output_dtypes2 = [emit.dtype for emit in flat_output_structure2]
 
     # Initialise List TensorArrays for output
     # First initialise them flat
-    flat_output_ta = [
+    flat_output_ta1 = [
         tensor_array_ops.TensorArray(
             dtype=dtype_i, dynamic_size=True, size=0, clear_after_read=False, name="rnn_output_%d" % i)
-        for i, dtype_i in enumerate(flat_output_dtypes)]
+        for i, dtype_i in enumerate(flat_output_dtypes1)]
     # Then turn it into a hierarchical form
-    output_ta = nest.pack_sequence_as(structure=current_output,
-                                    flat_sequence=flat_output_ta)
+    output_ta1 = nest.pack_sequence_as(structure=current_output1,
+                                    flat_sequence=flat_output_ta1)
+
+    flat_output_ta2 = [
+        tensor_array_ops.TensorArray(
+            dtype=dtype_i, dynamic_size=True, size=0, clear_after_read=False, name="rnn_output_%d" % i)
+        for i, dtype_i in enumerate(flat_output_dtypes2)]
+    # Then turn it into a hierarchical form
+    output_ta2 = nest.pack_sequence_as(structure=current_output2,
+                                    flat_sequence=flat_output_ta2)
 
     # This function track the stopping criterion
     # One all elements (i.e. batch iterations) are finished, the while loop will stop
-    def condition(_unused_time, elements_finished, *_):
-      return math_ops.logical_not(math_ops.reduce_all(elements_finished))
+    def condition(_unused_time, elements_finished1, *_):
+      return math_ops.logical_not(math_ops.reduce_all(elements_finished1))
 
-    def body(time, elements_finished, current_input, old_output, old_state,
-             output_ta):
+    def body(time, elements_finished1, elements_finished2, current_input1, current_input2, old_output1, old_output2, old_state1, old_state2, output_ta1,output_ta2):
       """Internal while loop body for raw_rnn.
 
       Args:
@@ -98,16 +111,19 @@ def flex_rnn(cell, loop_fn, init_fn=None, dtype=tf.float32,
 
       # Perform a new step
       current_time = time + 1
-      (next_finished, new_input, new_output, new_state) = loop_fn(
-         current_time, cell, current_input, old_output, old_state)
+      (next_finished1, new_input1, new_output1, new_state1) = loop_fn1(
+         current_time, cell1, current_input1, old_output1, old_state1, old_output2)
 
-      nest.assert_same_structure(old_state, new_state)
-      nest.assert_same_structure(old_output, new_output)
-      nest.assert_same_structure(current_input, new_input)
-      nest.assert_same_structure(output_ta, new_output)
+      (next_finished2, new_input2, new_output2, new_state2) = loop_fn2(
+         current_time, cell2, current_input2, old_output2, old_state2, old_output1)
+
+      nest.assert_same_structure(old_state1, new_state1)
+      nest.assert_same_structure(old_output1, new_output1)
+      nest.assert_same_structure(current_input1, new_input1)
+      nest.assert_same_structure(output_ta1, new_output1)
 
 
-      def _copy_some_through(current, candidate):
+      def _copy_some_through(current, candidate,elements_finished):
         current_flat = nest.flatten(current)
         candidate_flat = nest.flatten(candidate)
         result_flat = [
@@ -116,30 +132,42 @@ def flex_rnn(cell, loop_fn, init_fn=None, dtype=tf.float32,
         return nest.pack_sequence_as(
             structure=current, flat_sequence=result_flat)
 
-      current_output = _copy_some_through(old_output, new_output)
-      current_state = _copy_some_through(old_state, new_state)
+      current_output1 = _copy_some_through(old_output1, new_output1,elements_finished1)
+      current_state1 = _copy_some_through(old_state1, new_state1,elements_finished1)
+      current_output2 = _copy_some_through(old_output2, new_output2,elements_finished2)
+      current_state2 = _copy_some_through(old_state2, new_state2,elements_finished2)
 
-      current_output_flat = nest.flatten(current_output)
-      output_ta_flat = nest.flatten(output_ta)
+      current_output_flat1 = nest.flatten(current_output1)
+      output_ta_flat1 = nest.flatten(output_ta1)
+      current_output_flat2 = nest.flatten(current_output2)
+      output_ta_flat2 = nest.flatten(output_ta2)
 
-      elements_finished = math_ops.logical_or(elements_finished, next_finished)
+      elements_finished1 = math_ops.logical_or(elements_finished1, next_finished1)
+      elements_finished2 = math_ops.logical_or(elements_finished2, next_finished2)
 
-      output_ta_flat = [
+      output_ta_flat1 = [
           ta.write(time, emit)
-          for (ta, emit) in zip(output_ta_flat,current_output_flat)]
+          for (ta, emit) in zip(output_ta_flat1,current_output_flat1)]
+      output_ta_flat2 = [
+          ta.write(time, emit)
+          for (ta, emit) in zip(output_ta_flat2,current_output_flat2)]
 
-      output_ta = nest.pack_sequence_as(
-          structure=output_ta, flat_sequence=output_ta_flat)
 
-      return (current_time, elements_finished, new_input,
-              current_output, current_state, output_ta)
+      output_ta1 = nest.pack_sequence_as(
+          structure=output_ta1, flat_sequence=output_ta_flat1)
+      output_ta2 = nest.pack_sequence_as(
+          structure=output_ta2, flat_sequence=output_ta_flat2)
+
+
+      return (current_time, elements_finished1, elements_finished2, new_input1, new_input2,
+              current_output1,current_output2, current_state1, current_state2, output_ta1, output_ta2)
 
     returned = control_flow_ops.while_loop(
-        condition, body, loop_vars=[time, elements_finished, new_input,
-                                    current_output, current_state, output_ta],
+        condition, body, loop_vars=[time, elements_finished1, elements_finished2, new_input1, new_input2,
+                                    current_output1,current_output2, current_state1,current_state2, output_ta1,output_ta2],
         parallel_iterations=parallel_iterations,
         swap_memory=swap_memory)
 
-    (final_state, output_ta) = returned[-2:]
+    (final_state1,final_state2, output_ta1, output_ta2) = returned[-4:]
 
-    return (output_ta, final_state)
+    return (output_ta1, output_ta2 , final_state1 , final_state2)
