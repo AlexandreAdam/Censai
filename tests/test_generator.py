cfab0fb45ceae41d612d52f92509cf3caebb3026
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-03-22 09:43:37 -0400

    Corrected a bug in generator lensing code. Needed to use rotated and shifted coordinate for lens equations instead of underlying coordinates system

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,4 +1,5 @@
 from censai.data_generator import Generator, NISGenerator
+import tensorflow as tf
 
 def test_generator():
     gen = Generator()
@@ -15,18 +16,25 @@ def test_generator():
 
 
 def test_generator_NISGen_rim():
-    gen = NISGenerator(model="rim", batch_size=2)
-    for i, (kap, source, Y) in enumerate(gen):
+    gen = NISGenerator(model="rim", batch_size=1)
+    for i, (X, source, kap) in enumerate(gen):
         print(i)
 
-    print(kap.shape)
-    print(source.shape)
-    print(Y.shape)
-    return kap, source, Y
+    # kappa = tf.random.normal([1, 64, 64, 1])
+    source = tf.random.normal([1, 128, 128, 1])
+    r_ein = tf.constant(1, tf.float32)[tf.newaxis, tf.newaxis, tf.newaxis]
+    elp = tf.constant(0.2, tf.float32)[tf.newaxis, tf.newaxis, tf.newaxis]
+    phi = tf.constant(2, tf.float32)[tf.newaxis, tf.newaxis, tf.newaxis]
+    xlens = tf.constant(1, tf.float32)[tf.newaxis, tf.newaxis, tf.newaxis]
+    ylens = tf.constant(0, tf.float32)[tf.newaxis, tf.newaxis, tf.newaxis]
+
+    im_lensed = gen.lens_source(source, r_ein, elp, phi, xlens, ylens)
+
+    return X, source, kap, im_lensed
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    k, s, y = test_generator_NISGen_rim()
-    plt.imshow(y.numpy()[0, ..., 0])
+    x, s, k, im = test_generator_NISGen_rim()
+    plt.imshow(im.numpy()[0, ..., 0])
     plt.show()
 
