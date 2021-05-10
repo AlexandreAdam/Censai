547ed12e5ced8de3d03476bf78944bcc863f0b46
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-05-10 17:51:42 -0400

    Added regularizer to model

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 def test_ray_tracer_512():
     kappa = tf.random.normal(shape=[10, 512, 512, 1])
-    model = RayTracer512(initializer="glorot_uniform", bottleneck_kernel_size=8, bottleneck_strides=2, upsampling_interpolation=True)
+    model = RayTracer512(initializer="glorot_uniform", bottleneck_kernel_size=8, bottleneck_strides=2, upsampling_interpolation=True, decoder_encoder_filters=64)
     out = model(kappa)
 
 if __name__ == '__main__':
