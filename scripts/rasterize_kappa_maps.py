d493aee7eebcc36131c9e262ca8cc946d638ccfb
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-05-18 21:08:48 -0400

    Forgot a type enforcement

--- scripts/rasterize_kappa_maps.py
+++ scripts/rasterize_kappa_maps.py
@@ -135,7 +135,7 @@ parser.add_argument("--subhalo_id", required=True, type=str,
 parser.add_argument("--projection", required=True, type=str, help="2 characters, a combination of x, y and z (e.g. 'xy')")
 parser.add_argument("--base_filenames", default="kappa")
 parser.add_argument("--pixels", default=512, type=int, help="Number of pixels in the raster image")
-parser.add_argument("--n_neighbors", default=10, help="Number of neighbors used to compute kernel length")
+parser.add_argument("--n_neighbors", default=10, type=int, help="Number of neighbors used to compute kernel length")
 parser.add_argument("--offsets", default="/home/aadam/scratch/data/TNG300-1/offsets/offsets_099.hdf5")
 parser.add_argument("--snapshot_dir", default="/home/aadam/scratch/data/TNG300-1/snapshot99/", help="Root directory of the snapshot")
 parser.add_argument("--snapshot_id", default=99, type=int,
