31d7594a0f5e19224a9ad11e9dafcbaf00d91282
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-05-24 16:17:43 -0400

    Added adam grad update for RIM
    Former-commit-id: e47b5540b8bea216d4918aa1e0f9f3def5b9509e
    Former-commit-id: 35f2772637abd22b256b5e3e10b324af74e0b629 [formerly 0f9d59822b088aac3aa1261fa37106b658198afd]
    Former-commit-id: 691da4def92e2c4fb181484066096cbfb0c8de2f

--- censai/rim_unet.py
+++ censai/rim_unet.py
@@ -9,6 +9,10 @@ class RIM:
             batch_size,
             steps,
             pixels,
+            adam=True,
+            beta_1=0.9,
+            beta_2=0.999,
+            epsilon=1e-8,
             **models_kwargs):
         self.physical_model = physical_model
         self.pixels = pixels  # state size is not used, fixed in self.state_size_list
@@ -18,6 +22,10 @@ class RIM:
         self.model_1 = UnetModel(self.state_size_list, **models_kwargs)
         self.model_2 = UnetModel(self.state_size_list, **models_kwargs)
         self.batch_size = batch_size
+        self.adam = adam
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
 
         self.inputs_1 = tf.zeros(shape=(self.batch_size, self.pixels, self.pixels, 1))
         self.inputs_2 = tf.zeros(shape=(self.batch_size, self.pixels, self.pixels, 1))
@@ -51,6 +59,26 @@ class RIM:
     def output_size(self):
         return self._num_units
 
+    def grad_update(self, grad1, grad2, time_step):
+        if self.adam:
+            if time_step == 0:  # reset mean and variance for time t=-1
+                self._grad_mean1 = tf.zeros_like(grad1)
+                self._grad_var1 = tf.zeros_like(grad1)
+                self._grad_mean2 = tf.zeros_like(grad2)
+                self._grad_var2 = tf.zeros_like(grad2)
+            self._grad_mean1 = self. beta_1 * self._grad_mean1 + (1 - self.beta_1) * grad1
+            self._grad_var1  = self.beta_2 * self._grad_var1 + (1 - self.beta_2) * tf.square(grad1)
+            self._grad_mean2 = self. beta_1 * self._grad_mean2 + (1 - self.beta_1) * grad2
+            self._grad_var2  = self.beta_2 * self._grad_var2 + (1 - self.beta_2) * tf.square(grad2)
+            # for grad update, unbias the moments
+            m_hat1 = self._grad_mean1 / (1 - self.beta_1**(time_step + 1))
+            v_hat1 = self._grad_var1 / (1 - self.beta_2**(time_step + 1))
+            m_hat2 = self._grad_mean2 / (1 - self.beta_1**(time_step + 1))
+            v_hat2 = self._grad_var2 / (1 - self.beta_2**(time_step + 1))
+            return m_hat1 / (tf.sqrt(v_hat1) + self.epsilon), m_hat2 / (tf.sqrt(v_hat2) + self.epsilon)
+        else:
+            return grad1, grad2
+
     def __call__(self, inputs_1, state_1, grad_1, inputs_2, state_2, grad_2, scope=None):
         xt_1, ht_1 = self.model_1(inputs_1, state_1, grad_1)
         xt_2, ht_2 = self.model_2(inputs_2, state_2, grad_2)
@@ -65,18 +93,20 @@ class RIM:
             g.watch(self.inputs_2)
             cost = self.physical_model.log_likelihood(y_true=data, source=self.inputs_1, kappa=self.inputs_2)
         grads = g.gradient(cost, [self.inputs_1, self.inputs_2])
+        grads = self.grad_update(*grads, 0)
 
         output_1, state_1, output_2, state_2 = self.__call__(self.inputs_1, self.state_1, grads[0], self.inputs_2,
                                                              self.state_2, grads[1])
         output_series_1.append(output_1)
         output_series_2.append(output_2)
 
-        for current_step in range(self.steps - 1):
+        for current_step in range(1, self.steps):
             with tf.GradientTape() as g:
                 g.watch(output_1)
                 g.watch(output_2)
                 cost = self.physical_model.log_likelihood(y_true=data, source=output_1, kappa=output_2)
             grads = g.gradient(cost, [output_1, output_2])
+            grads = self.grad_update(*grads, current_step)
             output_1, state_1, output_2, state_2 = self.__call__(output_1, state_1, grads[0], output_2, state_2,
                                                                  grads[1])
             output_series_1.append(output_1)
