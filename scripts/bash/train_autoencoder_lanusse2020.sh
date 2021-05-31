7438ad78366fe093062e374a7fd7ca09b197bfed
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-05-31 19:56:28 -0400

    Added mirrored startegy to training script and updated training time
    Former-commit-id: b2867c1f10de02cd7edf490f62ecba73b4ce8ae1
    Former-commit-id: e90664763b713e05dd98073ab2c7f26dc569045b [formerly 361d7769c6c1ff9c4e8020fe615a4b4f714c0412]
    Former-commit-id: 7fecf3e13835cc9c4fd41d17344d07c975310d93

--- scripts/bash/train_autoencoder_lanusse2020.sh
+++ scripts/bash/train_autoencoder_lanusse2020.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --tasks=1
-#SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
-#SBATCH --gres=gpu:1
-#SBATCH --mem=8G			     # memory per node
-#SBATCH --time=0-01:00		# time (DD-HH:MM)
+#SBATCH --cpus-per-task=9 # maximum cpu per task is 3.5 per gpus
+#SBATCH --gres=gpu:3
+#SBATCH --mem=32G			     # memory per node
+#SBATCH --time=6-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Train_Autoencoder
 #SBATCH --output=%x-%j.out
