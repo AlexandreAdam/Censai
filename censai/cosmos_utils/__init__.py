1a13a62219cf884aab3407dcad565809d580b479
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-05-31 15:14:49 -0400

    Deleted old script, be careful when merging

---
+++ censai/cosmos_utils/__init__.py
@@ -0,0 +1,1 @@
+from .decode_cosmos import decode_function as decode
--- misc/iterative_inference_learning/layers/array_ops.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-def multi_slice(split_dim, slices, value, name='multi_slices'):
-  value_array = tf.unstack(value, axis=split_dim)
-  output_list = []
-  for s in slices:
-    output_list.append(tf.stack(value_array[s[0]:s[0]+s[1]],split_dim))
-
-  return output_list
