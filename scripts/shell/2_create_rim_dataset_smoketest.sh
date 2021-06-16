e75ed31b6dcfd0306dd8b6f7da05e2f90d2308eb
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-06-16 15:44:08 -0400

    Again a problem in argparse, should be the last

--- scripts/shell/2_create_rim_dataset_smoketest.sh
+++ scripts/shell/2_create_rim_dataset_smoketest.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=3  # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=1-00:10		 # time (DD-HH:MM)
+#SBATCH --time=0-00:10		 # time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Create-RIM-Dataset-SmokeTest
 #SBATCH --output=%x-%j.out
