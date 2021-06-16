a07bf2e798d730f1ff2a6288639986881ed4b283
Alexandre Adam <aadam@beluga4.int.ets1.calculquebec.ca>
2021-06-16 13:29:29 -0400

    Updated notebooks and shell scripts for beluga

--- scripts/shell/1_rasterize_kappa_maps_smoketest.sh
+++ scripts/shell/1_rasterize_kappa_maps_smoketest.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-#SBATCH --array=1-2
+#SBATCH --array=1
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=0-00:10		# time (DD-HH:MM)
-#SBATCH --account=def-lplevass
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-00:20		# time (DD-HH:MM)
+#SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Rasterize_Kappa_Maps
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python ../rasterize_kappa_maps.py \
   --output_dir=$HOME/scratch/Censai/data \
-  --subhalo_id=$HOME/scratch/Censai/data/subhalo_TNG100-1_id.npy \
-  --projection=xy \
-  --pixels=612 \
-  --smoke_test
+  --subhalo_id=$HOME/scratch/Censai/data/subhalo_TNG100-1_id.npy\
+  --groupcat_dir=$HOME/projects/rrg-lplevass/aadam/data/illustrisTNG100-1_snapshot99_groupcat/\
+  --snapshot_dir=$HOME/scratch/data/TNG100-1/snapshot99/\
+  --offsets=$HOME/scratch/data/TNG100-1/offsets/offsets_099.hdf5\
+  --snapshot_id=99\
+  --projection=xy\
+  --pixels=612\
+  --smoke_test\
+  --batch_size=20\
+  --use_gpu \
+  --smoke_test_id=52623
--- scripts/shell/1_rasterize_kappa_maps_yz.sh
+++ scripts/shell/1_rasterize_kappa_maps_yz.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-#SBATCH --array=1-20
+#SBATCH --array=1-10
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3  # maximum cpu per task is 3.5 per gpus
 #SBATCH --mem=32G			     # memory per node
+#SBATCH --gres=gpu:1
 #SBATCH --time=6-00:00		 # time (DD-HH:MM)
-#SBATCH --account=def-lplevass
+#SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Rasterize_Kappa_Maps
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python ../rasterize_kappa_maps.py\
   --output_dir=$HOME/scratch/Censai/data/kappa612_TNG100_64neighbors\
   --subhalo_id=$HOME/scratch/Censai/data/subhalo_TNG100-1_id.npy\
+  --groupcat_dir=$HOME/projects/rrg-lplevass/aadam/data/illustrisTNG100-1_snapshot99_groupcat/\
+  --snapshot_dir=$HOME/scratch/data/TNG100-1/snapshot99/\
+  --offsets=$HOME/scratch/data/TNG100-1/offsets/offsets_099.hdf5\
+  --snapshot_id=99\
   --projection=yz\
   --pixels=612\
   --fov=0.2\
   --n_neighbors=64\
-  --fw_param=3
+  --fw_param=3 \
+  --use_gpu \
+  --batch_size=20
