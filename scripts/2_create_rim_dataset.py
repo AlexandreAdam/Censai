9fdbeb617bcf73b7da959c722583624127c3fed3
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-06-18 09:21:01 -0400

    .
    Former-commit-id: 1bbf6b1d0e5953a84557a12a57b6b4e83799264b
    Former-commit-id: 40b452b73236c87aa2f1042aed172e0187235ebd [formerly 7faf2a4c882901c35dbc707742e96aa21faae7a3]
    Former-commit-id: 6212b9cdb4d0a86940a6bbe9f89ae3a6e901c0eb

--- scripts/2_create_rim_dataset.py
+++ scripts/2_create_rim_dataset.py
@@ -90,7 +90,7 @@ def distributed_strategy(args):
                 # Rough estimate of allowed rescaling factors
                 rescaling_array = np.linspace(min_theta_e/theta_e, max_theta_e/theta_e, args.rescaling_size) * sigma_crit_factor
                 # compute probability distribution of rescaling so that theta_e ~ Uniform(min_theta_e, max_theta_e)
-                rescaling_p = compute_rescaling_probabilities(kappa[j], rescaling_array,
+                rescaling_p = compute_rescaling_probabilities(kappa[j], rescaling_array, physical_pixel_scale,
                                                               sigma_crit, Dds=Dds, Ds=Ds, Dd=Dd,
                                                               bins=args.bins, min_theta_e=min_theta_e, max_theta_e=max_theta_e)
                 # make an informed random choice
