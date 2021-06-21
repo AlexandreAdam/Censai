ed03372c9e571fee7e14b96b32843feeb5811daf
Alexandre Adam <aadam@beluga4.int.ets1.calculquebec.ca>
2021-06-21 11:21:34 -0400

    Added shell script

--- scripts/shell/2_create_rim_dataset_200k.sh
+++ scripts/shell/2_create_rim_dataset_200k.sh
@@ -10,7 +10,7 @@
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python ../2_create_rim_dataset.py\
-  --output_dir=$HOME/scratch/Censai/data/lens_TNG100/\
+  --output_dir=$HOME/scratch/Censai/data/lenses_TNG100/\
   --len_dataset=200000\
   --kappa_dir=$HOME/scratch/Censai/data/kappa612_TNG100_64neighbors\
   --cosmos_dir=$HOME/scratch/Censai/data/cosmos_23.5/\
