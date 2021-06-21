b0b1f102d1066667b697f533fe573ac835ac7646
Alexandre Adam <aadam@beluga4.int.ets1.calculquebec.ca>
2021-06-21 11:21:34 -0400

    Added shell script
    Former-commit-id: ed03372c9e571fee7e14b96b32843feeb5811daf
    Former-commit-id: af4321ddd52a5906935a44cd30695e0547a6679e [formerly 0e17e1c07dc5eee5701a0f2d2b92543c59661de8]
    Former-commit-id: 9ad70d52e391e99e5c7bee7994f55bdb01615554

--- scripts/shell/2_create_rim_dataset_5k_test_set.sh
+++ scripts/shell/2_create_rim_dataset_5k_test_set.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --array=1-20
+#SBATCH --array=1-10
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=1-00:00		# time (DD-HH:MM)
+#SBATCH --time=00-02:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Create-RIM-Dataset-200k
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python ../2_create_rim_dataset.py\
-  --output_dir=$HOME/scratch/Censai/data/lenses_TNG100/\
-  --len_dataset=200000\
+  --output_dir=$HOME/scratch/Censai/data/lenses_TNG100_testset/\
+  --len_dataset=5000\
   --kappa_dir=$HOME/scratch/Censai/data/kappa612_TNG100_64neighbors\
   --cosmos_dir=$HOME/scratch/Censai/data/cosmos_23.5/\
   --src_pixels=128\
