5c3f176917618ed77e0c1c777dccdc66b67e8c90
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-06-28 16:05:24 -0400

    Refactored smoketest with nis

--- scripts/shell/4_train_rim_unet_smoketest_nis.sh
+++ scripts/shell/4_train_rim_unet_smoketest_nis.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=5 # maximum cpu per task is 3.5 per gpus
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --mem=16G			     # memory per node
-#SBATCH --time=0-01:00		# time (DD-HH:MM)
+#SBATCH --time=0-05:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Train_RIM_SmokeTest_NIS
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python ../4_train_rim_unet.py\
-  --epochs=10\
-  --total_items=50\
-  --batch_size=5\
-  --initial_learning_rate=1e-4\
-  --decay_rate=1\
-  --pixels=64\
-  --kappalog=True\
+  --time_steps=16\
   --adam=True\
-  --logdir=$HOME/scratch/Censai/logs\
-  --model_dir=$HOME/scratch/Censai/models\
+  --kappalog=True\
+  --normalize=False\
+  --kappa_strides=4\
+  --source_strides=2\
+  --state_size_1=4\
+  --state_size_2=32\
+  --state_size_3=128\
+  --state_size_4=512\
+  --forward_method=conv2d\
+  --batch_size=4\
+  --datasets=$HOME/scratch/Censai/data/lenses_NIS/\
+  --train_split=0.9\
+  --total_items=2000\
+  --num_parallel_reads=20\
+  --cache_file=$SLURM_TMPDIR/cache\
+  --epochs=10\
+  --initial_learning_rate=1e-3\
+  --decay_rate=1\
+  --staircase\
+  --clipping=True\
+  --logdir=$HOME/scratch/Censai/logs/\
+  --model_dir=$HOME/scratch/Censai/models/\
+  --logname=RIM_SmokeTest\
   --checkpoints=1\
   --max_to_keep=1\
-  --logname=RIM_smoketest_NIS
+  --seed=42
