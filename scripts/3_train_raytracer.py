cd471a0d680fc0175f450c7fee5dbaaf0a3a95c9
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-06-28 15:29:07 -0400

    Fixed argument problem in analytical dataset

--- scripts/3_train_raytracer.py
+++ scripts/3_train_raytracer.py
@@ -211,50 +211,49 @@ if __name__ == "__main__":
     parser.add_argument("--compression_type",           default=None,                help="Compression type used to write data. Default assumes no compression.")
 
     # Model hyper parameters
-    parser.add_argument("--initializer",                    default="glorot_uniform", type=str,   help="Weight initializer")
-    parser.add_argument("--decoder_encoder_kernel_size",    default=3,                type=int,   help="Main kernel size")
-    parser.add_argument("--pre_bottleneck_kernel_size",     default=6,                type=int,   help="Kernel size of layer before bottleneck")
-    parser.add_argument("--bottleneck_kernel_size",         default=16,               type=int,   help="Kernel size of bottleneck layr, should be twice bottleneck feature map size")
-    parser.add_argument("--bottleneck_strides",             default=4,                type=int,   help="Strided of the downsampling convolutional layer before bottleneck")
-    parser.add_argument("--decoder_encoder_filters",        default=32,               type=int,   help="Number of filters of conv layers")
-    parser.add_argument("--filter_scaling",                 default=1,                type=float, help="Scaling of the number of filters at each layers (1=no scaling)")
-    parser.add_argument("--upsampling_interpolation",       default=False,            type=bool,  help="True: Use Bilinear interpolation for upsampling, False use Fractional Striding Convolution")
-    parser.add_argument("--activation",                     default="linear",         type=str,   help="Non-linearity of layers")
-    parser.add_argument("--kernel_regularizer_amp",         default=1e-3,             type=float, help="l2 regularization on weights")
-    parser.add_argument("--kappalog",                       default=True,             type=bool,  help="Input is log of kappa")
-    parser.add_argument("--normalize",                      default=False,            type=bool,  help="Normalize log of kappa with max and minimum values defined in definitions.py")
+    parser.add_argument("--initializer",                    default="glorot_uniform", type=str,     help="Weight initializer")
+    parser.add_argument("--decoder_encoder_kernel_size",    default=3,                type=int,     help="Main kernel size")
+    parser.add_argument("--pre_bottleneck_kernel_size",     default=6,                type=int,     help="Kernel size of layer before bottleneck")
+    parser.add_argument("--bottleneck_kernel_size",         default=16,               type=int,     help="Kernel size of bottleneck layr, should be twice bottleneck feature map size")
+    parser.add_argument("--bottleneck_strides",             default=4,                type=int,     help="Strided of the downsampling convolutional layer before bottleneck")
+    parser.add_argument("--decoder_encoder_filters",        default=32,               type=int,     help="Number of filters of conv layers")
+    parser.add_argument("--filter_scaling",                 default=1,                type=float,   help="Scaling of the number of filters at each layers (1=no scaling)")
+    parser.add_argument("--upsampling_interpolation",       default=False,            type=bool,    help="True: Use Bilinear interpolation for upsampling, False use Fractional Striding Convolution")
+    parser.add_argument("--activation",                     default="linear",         type=str,     help="Non-linearity of layers")
+    parser.add_argument("--kernel_regularizer_amp",         default=1e-3,             type=float,   help="l2 regularization on weights")
+    parser.add_argument("--kappalog",                       default=True,             type=bool,    help="Input is log of kappa")
+    parser.add_argument("--normalize",                      default=False,            type=bool,    help="Normalize log of kappa with max and minimum values defined in definitions.py")
 
     # Training set params
-    parser.add_argument("-b", "--batch_size",               default=10,     type=int,       help="Number of images in a batch")
-    parser.add_argument("--train_split",                    default=0.8,    type=float,     help="Fraction of the training set")
-    parser.add_argument("--total_items",                    required=True,  type=int,       help="Total images in an epoch.")
+    parser.add_argument("-b", "--batch_size",               default=10,     type=int,               help="Number of images in a batch")
+    parser.add_argument("--train_split",                    default=0.8,    type=float,             help="Fraction of the training set")
+    parser.add_argument("--total_items",                    required=True,  type=int,               help="Total images in an epoch.")
     # ... for tfrecord dataset
-    parser.add_argument("--num_parallel_reads",             default=10,     type=int,       help="TFRecord dataset number of parallel reads when loading data")
-    parser.add_argument("--cache_file",                     default=None,                   help="Path to cache file, useful when training on server. Use ${SLURM_TMPDIR}/cache")
-    parser.add_argument("--cycle_length",       default=4,      type=int,   help="Number of files to read concurrently.")
-    parser.add_argument("--block_length",       default=1,      type=int,   help="Number of example to read from each files.")
+    parser.add_argument("--num_parallel_reads",             default=10,     type=int,               help="TFRecord dataset number of parallel reads when loading data")
+    parser.add_argument("--cache_file",                     default=None,                           help="Path to cache file, useful when training on server. Use ${SLURM_TMPDIR}/cache")
+    parser.add_argument("--cycle_length",                   default=4,      type=int,               help="Number of files to read concurrently.")
+    parser.add_argument("--block_length",                   default=1,      type=int,               help="Number of example to read from each files.")
 
     # Logs
-    parser.add_argument("--logdir",                         default="None",        help="Path of logs directory.")
-    parser.add_argument("--logname",                        default=None,          help="Name of the logs, default is 'RT_' + date")
-    parser.add_argument("--model_dir",                      default="None",        help="Directory where to save model weights")
-    parser.add_argument("--checkpoints",                    default=10, type=int,  help="Save a checkpoint of the models each {%} iteration")
-    parser.add_argument("--max_to_keep",                    default=3, type=int,   help="Max model checkpoint to keep")
+    parser.add_argument("--logdir",                         default="None",                         help="Path of logs directory.")
+    parser.add_argument("--logname",                        default=None,                           help="Name of the logs, default is 'RT_' + date")
+    parser.add_argument("--model_dir",                      default="None",                         help="Directory where to save model weights")
+    parser.add_argument("--checkpoints",                    default=10,     type=int,               help="Save a checkpoint of the models each {%} iteration")
+    parser.add_argument("--max_to_keep",                    default=3,      type=int,               help="Max model checkpoint to keep")
 
     # Optimization params
-    parser.add_argument("-e", "--epochs",                   default=10,     type=int,   help="Number of epochs for training.")
-    parser.add_argument("--initial_learning_rate",          default=1e-3,   type=float, help="Initial learning rate.")
-    parser.add_argument("--decay_rate",                     default=1.,     type=float, help="Exponential decay rate of learning rate (1=no decay).")
-    parser.add_argument("--decay_steps",                    default=1000,   type=int,   help="Decay steps of exponential decay of the learning rate.")
-    parser.add_argument("--clipping",                       default=True,   type=bool,  help="Clip backprop gradients between -10 and 10")
-    parser.add_argument("--patience",                       default=np.inf, type=int, help="Number of step at which training is stopped if no improvement is recorder")
-    parser.add_argument("--tolerance",                      default=0,      type=float, help="Current score <= (1 - tolerance) * best score => reset patience, else reduce patience.")
+    parser.add_argument("-e", "--epochs",                   default=10,     type=int,               help="Number of epochs for training.")
+    parser.add_argument("--initial_learning_rate",          default=1e-3,   type=float,             help="Initial learning rate.")
+    parser.add_argument("--decay_rate",                     default=1.,     type=float,             help="Exponential decay rate of learning rate (1=no decay).")
+    parser.add_argument("--decay_steps",                    default=1000,   type=int,               help="Decay steps of exponential decay of the learning rate.")
+    parser.add_argument("--clipping",                       default=True,   type=bool,              help="Clip backprop gradients between -10 and 10")
+    parser.add_argument("--patience",                       default=np.inf, type=int,               help="Number of step at which training is stopped if no improvement is recorder")
+    parser.add_argument("--tolerance",                      default=0,      type=float,             help="Current score <= (1 - tolerance) * best score => reset patience, else reduce patience.")
 
     # Reproducibility params
-    parser.add_argument("--seed", default=None, type=int, help="Random seed for numpy and tensorflow")
-    parser.add_argument("--json_override", default=None,
-                        help="A json filepath that will override every command line parameters. "
-                             "Useful for reproducibility")
+    parser.add_argument("--seed",                           default=None,   type=int, help="Random seed for numpy and tensorflow")
+    parser.add_argument("--json_override",                  default=None,             help="A json filepath that will override every command line parameters. "
+                                                                                           "Useful for reproducibility")
 
     args = parser.parse_args()
     if args.seed is not None:
