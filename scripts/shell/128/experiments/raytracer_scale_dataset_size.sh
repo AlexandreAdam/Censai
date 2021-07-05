c38d988f2ca6fb4503d0a2ad3c3a428ee26bd089
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-07-05 15:24:17 -0400

    Fixed gridsearch sheel scripts
    Former-commit-id: 813f86fe03fb00700f21e56e686012a00d3d7d50
    Former-commit-id: 78c3e06b161de87eb1badf9d7b6d53a22722315b [formerly ba58be102eada443af6303fbc59afa9893c32a9a]
    Former-commit-id: bc20e6c2fa96504e7b6bc368680d48cbae0891ed

--- scripts/shell/128/experiments/raytracer_scale_dataset_size.sh
+++ scripts/shell/128/experiments/raytracer_scale_dataset_size.sh
@@ -6,30 +6,31 @@
 #SBATCH --mem=16G			     # memory per node
 #SBATCH --time=1-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Train_RayTracer128_TNG100
+#SBATCH --job-name=Train_RayTracer128_TNG100_ScaleDatasetSize
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
-python $CENSAI_PATH/scripts/train_raytracer.py\
+python $CENSAI_PATH/scripts/experiments/raytracer_gridsearch.py\
   --datasets $HOME/scratch/Censai/data/alpha128_TNG100\
   --compression_type=GZIP\
-  --total_items=10 100 500 1000 2000 5000 10000 30000 40000\
+  --strategy=exhaustive\
+  --total_items 10 100 500 1000 2000 5000 10000 30000 40000\
   --epochs=5000\
   --train_split=0.9\
   --pixels=128\
-  --kernel_size=5\
   --initial_learning_rate=1e-4\
   --decay_rate=0.9\
   --decay_steps=1000\
-  --filters=32\
-  --filter_scaling=1\
-  --layers=4\
-  --block_conv_layers=3\
-  --strides=2\
-  --resampling_kernel_size=5\
+  --kernel_size 5\
+  --filters 32\
+  --filter_scaling 1\
+  --layers 4\
+  --block_conv_layers 3\
+  --strides 2\
+  --resampling_kernel_size 5\
   --kappalog\
   --upsampling_interpolation\
-  --kernel_regularizer_amp=0\
-  --initializer=glorot_uniform\
+  --kernel_regularizer_amp 0\
+  --initializer glorot_uniform\
   --batch_size=10\
   --num_parallel_reads=10\
   --cycle_length=10\
--- scripts/shell/128/experiments/raytracer_scale_filters.sh
+++ scripts/shell/128/experiments/raytracer_scale_filters.sh
@@ -6,38 +6,39 @@
 #SBATCH --mem=16G			     # memory per node
 #SBATCH --time=0-10:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Train_RayTracer128_TNG100
+#SBATCH --job-name=Train_RayTracer128_TNG100_ScaleFilters
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
-python $CENSAI_PATH/scripts/train_raytracer.py\
-  --datasets $HOME/scratch/Censai/data/alpha128_TNG100\
+python $CENSAI_PATH/scripts/experiments/raytracer_gridsearch.py\
+  --datasets $CENSAI_PATH/data/alpha128_TNG100\
   --compression_type=GZIP\
-  --total_items=10 100 1000\
+  --strategy=exhaustive\
+  --total_items 10 100 1000\
   --epochs=5000\
   --train_split=0.9\
   --pixels=128\
-  --kernel_size=5\
   --initial_learning_rate=1e-4\
+  --kernel_size 5\
   --decay_rate=0.9\
   --decay_steps=1000\
-  --filters=8 16 32 64\
-  --filter_scaling=1 2\
-  --layers=4\
-  --block_conv_layers=3\
-  --strides=2\
-  --resampling_kernel_size=5\
+  --filters 8 16 32 64\
+  --filter_scaling 1 2\
+  --layers 4\
+  --block_conv_layers 3\
+  --strides 2\
+  --resampling_kernel_size 5\
   --kappalog\
   --upsampling_interpolation\
-  --kernel_regularizer_amp=0\
-  --initializer=glorot_uniform\
+  --kernel_regularizer_amp 0\
+  --initializer glorot_uniform\
   --batch_size=10\
   --num_parallel_reads=10\
   --cycle_length=10\
   --block_length=1\
   --cache_file=$SLURM_TMPDIR/cache\
-  --logdir=$HOME/scratch/Censai/logs\
+  --logdir=$CENSAI_PATH/logs\
   --logname_prefixe=RayTracer128_ScaleFilters\
-  --model_dir=$HOME/scratch/Censai/models\
+  --model_dir=$CENSAI_PATH/models\
   --checkpoints=5\
   --max_to_keep=3\
   --n_residuals=5\
