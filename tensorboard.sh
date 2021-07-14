7938e0f0380ff65a715b01da0fe3a3db876e5210
Alexandre Adam <aadam@beluga1.int.ets1.calculquebec.ca>
2021-07-14 19:47:28 -0400

    tensorboard
    Former-commit-id: b67a81ae1e99347f7c29c83e712d69afe2fa3f85
    Former-commit-id: 0cc108574c8b7f807beec85196c8382e5e85615c [formerly 45ba7c8c38a310b75e7af69ef7a87d35702248be]
    Former-commit-id: e033eb5ea5fcce38dfa500d02bc27bac7373cd95

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3                     # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50G                            # memory per node
-#SBATCH --time=01:00                         # time (DD-HH:MM)
+#SBATCH --time=1:00:00                         # time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Tensorboard
 #SBATCH --output=%x-%j.out
