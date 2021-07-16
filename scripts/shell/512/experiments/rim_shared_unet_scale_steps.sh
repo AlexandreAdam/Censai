94157b76b57581a9c9a048ca51e2d2080ffff59a
AlexandreAdam <adam.alexandre01123@gmail.com>
2021-07-16 10:39:49 -0400

    args

--- scripts/shell/512/experiments/rim_shared_unet_scale_steps.sh
+++ scripts/shell/512/experiments/rim_shared_unet_scale_steps.sh
@@ -22,7 +22,7 @@ python $CENSAI_PATH/scripts/experiments/rim_shared_unet_gridsearch.py\
   --tolerance=0.01\
   --batch_size=1\
   --train_split=0.9\
-  --total_items 100\
+  --total_items 1000\
   --num_parallel_reads=1\
   --cycle_length=1\
   --block_length=1\
