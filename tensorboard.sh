7236b8ed0f900d8f013161803b69bd392f73f525
Alexandre Adam <aadam@beluga1.int.ets1.calculquebec.ca>
2021-08-01 13:13:56 -0400

    updated notebooks and tensorboard shell
    Former-commit-id: 4f671c3ea82edd566a8cc17f36cbb5d0fc30a0e8
    Former-commit-id: db0257dee202a1636383522df7d5f5905402c81b [formerly 99879a1057f9dddbffd32bbe8ceb1ffd0cc9770d]
    Former-commit-id: 7c996f28b8cc2d1114035d68a31ac9a8623bf1c3

--- tensorboard.sh
+++ tensorboard.sh
@@ -3,8 +3,8 @@
 #SBATCH --cpus-per-task=3                     # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50G                            # memory per node
-#SBATCH --time=3:00:00                         # time (DD-HH:MM)
-#SBATCH --account=rrg-lplevass
+#SBATCH --time=2:59:00                         # time (DD-HH:MM)
+#SBATCH --account=def-lplevass
 #SBATCH --job-name=Tensorboard
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
