d69238c9f4e4cb18d7bc6322127f856dfa0a1389
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-08-02 09:08:41 -0400

    Coded a simple version of resnet autoencoder

--- censai/models/__init__.py
+++ censai/models/__init__.py
@@ -4,4 +4,5 @@ from .rim_model import Model
 from .rim_unet_model512 import UnetModel512
 from .rim_unet_model import UnetModel
 from .cosmos_autoencoder import CosmosAutoencoder
+from .resnet_autoencoder import ResnetAutoencoder
 from .rim_shared_unet_model import SharedUnetModel
