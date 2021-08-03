57bc5585e312061cc9ef70ce6a9308d1dd3a05d3
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-08-03 00:11:28 -0400

    Updated the way I build the model

--- scripts/train_kappa_resnetvae_first_stage.py
+++ scripts/train_kappa_resnetvae_first_stage.py
@@ -67,7 +67,7 @@ def main(args):
         batch_norm=args.batch_norm,
         latent_size=args.latent_size
     )
-    vae.build(input_shape=[args.batch_size, pixels, pixels, 1])
+    vae.call(tf.zeros(shape=[args.batch_size, pixels, pixels, 1]))  # build layers of model
     learning_rate_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=args.initial_learning_rate,
         decay_rate=args.decay_rate,
