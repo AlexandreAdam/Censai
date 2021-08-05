79cb89e57857e1fce6d79d22037295a9264a70fb
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-08-05 16:43:33 -0400

    Changed paths

--- scripts/shell/512_k256/1_rasterize_kappa_maps_yz.sh
+++ scripts/shell/512_k256/1_rasterize_kappa_maps_yz.sh
@@ -12,9 +12,9 @@ source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/rasterize_kappa_maps.py\
   --output_dir=$HOME/scratch/Censai/data/kappa356_TNG100_512\
   --subhalo_id=$HOME/scratch/Censai/data/subhalo_TNG100-1_id.npy\
-  --groupcat_dir=$HOME/projects/rrg-lplevass/aadam/data/illustrisTNG100-1_snapshot99_groupcat/\
-  --snapshot_dir=$HOME/scratch/data/TNG100-1/snapshot99/\
-  --offsets=$HOME/scratch/data/TNG100-1/offsets/offsets_099.hdf5\
+  --groupcat_dir=$HOME/projects/rrg-lplevass/data/TNG100-1/groupcat99/\
+  --snapshot_dir=$HOME/projects/rrg-lplevass/TNG100-1/snapshot99/\
+  --offsets=$HOME/projects/rrg-lplevass/TNG100-1/offsets/offsets_099.hdf5\
   --snapshot_id=99\
   --projection=yz\
   --pixels=356\
