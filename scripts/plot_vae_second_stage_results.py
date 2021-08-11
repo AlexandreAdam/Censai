0cc5f1bb5ab2d21c1649a0d0654a3c73f8e998cb
AlexandreAdam <adam.alexandre01123@gmail.com>
2021-08-11 16:24:14 -0400

    .

---
+++ scripts/plot_vae_second_stage_results.py
@@ -0,0 +1,52 @@
+from censai.models import VAE, VAESecondStage
+from censai.utils import sampling_plot
+import numpy as np
+import tensorflow as tf
+import os, glob
+import json
+
+
+def main(args):
+    model_list = glob.glob(os.path.join(os.getenv("CENSAI_PATH"), "models", args.model_prefixe, "*"))
+    for model in model_list:
+        if "second_stage" in model:
+            continue
+        with open(os.path.join(model, "model_hparams.json"), "r") as f:
+            vae_hparams = json.load(f)
+
+        # load weights of first stage
+        vae = VAE(**vae_hparams)
+        ckpt1 = tf.train.Checkpoint(net=vae)
+        checkpoint_manager1 = tf.train.CheckpointManager(ckpt1, model)
+        checkpoint_manager1.checkpoint.restore(checkpoint_manager1.latest_checkpoint).expect_partial()
+
+        second_stages = [file for file in model_list if "second_stage" in file and model in file]
+        for second_stage in second_stages:
+            with open(os.path.join(second_stage, "model_hparams.json"), "r") as f:
+                vae2_hparams = json.load(f)
+            vae2 = VAESecondStage(**vae2_hparams)
+            ckpt2 = tf.train.Checkpoint(net=vae2)
+            checkpoint_manager2 = tf.train.CheckpointManager(ckpt2, second_stage)
+            checkpoint_manager2.checkpoint.restore(checkpoint_manager2.latest_checkpoint).expect_partial()
+
+            for n in range(args.n_plots):
+                z = vae2.sample(args.sampling_size)
+                y_pred = vae.decode(z)
+                fig = sampling_plot(y_pred)
+                fig.savefig(os.path.join(os.getenv("CENSAI_PATH"), "results", "vae2_sampling" + model + args.output_postfixe + f"{n:01d}.png"))
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--model_prefixe",      required=True)
+    parser.add_argument("--output_postfixe",    default="",         help="phrase to append to result filename")
+    parser.add_argument("--sampling_size",      default=81,         help="Number of images to sample for a single figure")
+    parser.add_argument("--n_plots",            default=1,          help="Number of plot to make for a given model")
+    parser.add_argument("--seed",               default=None)
+
+    args = parser.parse_args()
+
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    main(args)
