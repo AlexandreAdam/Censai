1c8ef7297fde9e3356a8aaa5148d06e0819136f5
Alexandre Adam <aadam@beluga1.int.ets1.calculquebec.ca>
2021-09-14 00:05:51 -0400

    beluga
    Former-commit-id: 8d0ec69fd2f1e3b7a28bea3dfead35c9126038a1
    Former-commit-id: 30fbf980711e2a7de8c4260bf8359cc80ab3f92c [formerly 7204d5579b5799fa604b374ae243a03f56519031]
    Former-commit-id: 1acb95be95c8bdb09b4d321efa5e41b511a2e702

--- scripts/shell/512_k128/1_rasterize_halo_kappa_maps_xy_smoketest.sh
+++ scripts/shell/512_k128/1_rasterize_halo_kappa_maps_xy_smoketest.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-#SBATCH --array=1-10
+#!/bin/bash 
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3  # maximum cpu per task is 3.5 per gpus
-#SBATCH --mem=64G			     # memory per node
+#SBATCH --mem=32G			     # memory per node
 #SBATCH --gres=gpu:1
-#SBATCH --time=1-00:00		 # time (DD-HH:MM)
+#SBATCH --time=0-01:00		 # time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Rasterize_Kappa_Maps_512_228_yz
+#SBATCH --job-name=Rasterize_Kappa_Maps_512_k228_xy
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/rasterize_halo_kappa_maps.py\
@@ -16,10 +15,11 @@ python $CENSAI_PATH/scripts/rasterize_halo_kappa_maps.py\
   --snapshot_dir=$HOME/projects/rrg-lplevass/data/TNG100-1/snapshot99/\
   --offsets=$HOME/projects/rrg-lplevass/data/TNG100-1/offsets/offsets_099.hdf5\
   --snapshot_id=99\
-  --projection=yz\
+  --projection=xy\
   --pixels=158\
   --fov=0.2\
   --n_neighbors=64\
   --fw_param=3\
   --use_gpu\
-  --batch_size=5
+  --batch_size=5\
+  --smoke_test
