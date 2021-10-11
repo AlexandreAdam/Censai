02bc6e3e6a60790213fe5e7992cc6faae22d8d2b
Alexandre Adam <aadam@beluga2.int.ets1.calculquebec.ca>
2021-10-11 16:03:18 -0400

    beluga and script changes

--- scripts/shell/final_run/2_create_hkappa_dataset.sh
+++ scripts/shell/final_run/2_create_hkappa_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=0-00:30		# time (DD-HH:MM)
+#SBATCH --time=0-02:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Create-Kappa-Dataset-20k
 #SBATCH --output=%x-%j.out
--- scripts/shell/final_run/gridsearch/vae_cosmos.sh
+++ scripts/shell/final_run/gridsearch/vae_cosmos.sh
@@ -11,8 +11,8 @@
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/experiments/vae_cosmos_gridsearch.py\
   --datasets $CENSAI_PATH/data/cosmos_23.5_finalrun_train/\
-  --strategy=exhaustive\
-  --epochs=200\
+  --strategy=uniform\
+  --epochs=1000\
   --n_models=32\
   --batch_size 20\
   --train_split=0.9\
@@ -20,13 +20,13 @@ python $CENSAI_PATH/scripts/experiments/vae_cosmos_gridsearch.py\
   --optimizer Adam\
   --initial_learning_rate 1e-4\
   --decay_rate 0.5\
-  --decay_steps=10000\
-  --beta_init=0\
-  --beta_end_value=0.1\
+  --decay_steps=30000\
+  --beta_init 0.1\
+  --beta_end_value 0.1 0.2 0.3 0.4 0.5 0.6 0.9 1.\
   --beta_decay_power 0.5\
-  --beta_decay_steps=20000\
+  --beta_decay_steps 50000 100000\
   --beta_cyclical 0\
-  --skip_strength_init 0. 1e-3\
+  --skip_strength_init 0\
   --skip_strength_end_value=0.\
   --skip_strength_decay_power 0.5\
   --skip_strength_decay_steps=2000\
@@ -36,22 +36,22 @@ python $CENSAI_PATH/scripts/experiments/vae_cosmos_gridsearch.py\
   --l2_bottleneck_decay_steps=10000\
   --staircase\
   --clipping\
-  --patience=40\
+  --patience=60\
   --tolerance=0.01\
   --block_length=1\
   --layers 3 4\
-  --conv_layers 3 4\
+  --conv_layers 2 3\
   --filter_scaling 2\
   --filters 16 32\
   --kernel_size 3\
-  --kernel_reg_amp=1e-4\
-  --bias_reg_amp=1e-4\
+  --kernel_reg_amp=0\
+  --bias_reg_amp=0\
   --activation leaky_relu bipolar_relu\
   --batch_norm 0\
-  --latent_size 64 84 128 \
+  --latent_size 64 256\
   --cache_file=$SLURM_TMPDIR/cache\
-  --logdir=$CENSAI_PATH/logsVAE_cosmos\
-  --logname_prefixe=VAE1_cosmos_HPARAMS_1\
+  --logdir=$CENSAI_PATH/logsFR\
+  --logname_prefixe=VAE1_COSMOS_O\
   --model_dir=$CENSAI_PATH/models\
   --checkpoints=5\
   --max_to_keep=10\
