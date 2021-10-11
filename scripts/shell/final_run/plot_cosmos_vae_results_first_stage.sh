094abd0d16b051db74ab408dfd4da56dba599bd3
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-11 14:43:40 -0400

    .

---
+++ scripts/shell/final_run/plot_cosmos_vae_results_first_stage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G			     # memory per node
+#SBATCH --time=0-01:00		# time (DD-HH:MM)
+#SBATCH --account=rrg-lplevass
+#SBATCH --job-name=PlotResults_VAE_FirstStage
+#SBATCH --output=%x-%j.out
+source $HOME/environments/censai3.8/bin/activate
+python $CENSAI_PATH/scripts/plot_vae_first_stage_results.py\
+  --model_prefixe=VAE1_cosmos_HPARAMS_1\
+  --dataset=$CENSAI_PATH/data/cosmos_23.5_finalrun_train/\
+  --type=cosmos\
+  --batch_size=30\
+  --sampling_size=81\
+  --n_plots=5\
+  --seed=42
