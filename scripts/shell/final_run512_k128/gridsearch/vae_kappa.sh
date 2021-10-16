33f4902ee2f3ed730cd3381ebe64284468654934
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-16 16:01:09 -0400

    Changed hparams of vae i final runs to match best model in previous ones
    Former-commit-id: ebd87a1b08ec400e14461f89a72ba23285608f4f
    Former-commit-id: daa0e8f8237cea39167067ef5c6c5733d5c0cac7 [formerly 780887c54071a4d7077172771210c53e473a741c]
    Former-commit-id: b213a2121ab3e56c8afffcd4a7d728e4c01a55de

--- scripts/shell/final_run512_k128/gridsearch/vae_kappa.sh
+++ scripts/shell/final_run512_k128/gridsearch/vae_kappa.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-#SBATCH --array=1-18
+#SBATCH --array=1-32
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=2-00:00		# time (DD-HH:MM)
+#SBATCH --time=0-06:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Train_VAE_hkappa_Grid_Hparams
+#SBATCH --job-name=Train_VAE_hkappa_FR128
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/experiments/vae_kappa_gridsearch.py\
-  --datasets $CENSAI_PATH/data/hkappa512_TNG100_trainset/ $CENSAI_PATH/data/kappa512_TNG100_trainset/\
+  --datasets $CENSAI_PATH/data/hkappa128_TNG100_trainset_fr/\
   --compression_type=GZIP\
-  --strategy=uniform\
-  --epochs=1000\
-  --n_models=18\
-  --batch_size 20\
+  --strategy=exhaustive\
+  --epochs=100\
+  --n_models=32\
+  --batch_size 32\
   --train_split=0.9\
-  --total_items 70000\
+  --total_items 50000\
   --optimizer Adam\
-  --initial_learning_rate 1e-4 1e-5\
-  --decay_rate 0.5\
-  --decay_steps=50000\
+  --initial_learning_rate 1e-4\
+  --decay_rate 0.7\
+  --decay_steps=10000\
   --staircase\
   --beta_init 0.1\
-  --beta_end_value 0.3 0.4 0.5 0.6 0.7 1\
+  --beta_end_value 0.3 0.6 1 1.1\
   --beta_decay_power 1\
-  --beta_decay_steps 50000 100000 200000\
+  --beta_decay_steps 10000 20000\
   --beta_cyclical 0\
   --skip_strength_init 0\
   --skip_strength_end_value=0.\
@@ -37,8 +37,8 @@ python $CENSAI_PATH/scripts/experiments/vae_kappa_gridsearch.py\
   --l2_bottleneck_decay_power 0.5\
   --l2_bottleneck_decay_steps=10000\
   --clipping\
-  --patience=40\
-  --tolerance=0.01\
+  --patience=100\
+  --tolerance=0.0\
   --block_length=1\
   --layers 4\
   --conv_layers 1\
@@ -48,11 +48,11 @@ python $CENSAI_PATH/scripts/experiments/vae_kappa_gridsearch.py\
   --kernel_reg_amp=0\
   --bias_reg_amp=0\
   --activation leaky_relu\
-  --batch_norm 0\
-  --latent_size 84\
+  --batch_norm 0 1\
+  --latent_size 84 156\
   --cache_file=$SLURM_TMPDIR/cache\
   --logdir=$CENSAI_PATH/logsFR512k128\
-  --logname_prefixe=VAE1_hk512O\
+  --logname_prefixe=VAE1_hk128fr\
   --model_dir=$CENSAI_PATH/models\
   --checkpoints=5\
   --max_to_keep=3\
