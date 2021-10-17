d0ccbb5ae859db42eeb71d642a61f2b730a736e8
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-17 12:21:31 -0400

    .
    Former-commit-id: 8b1d4ee1f394195b5c16c7de5d96ae96cc84b696
    Former-commit-id: ea3d14fcd0f31bae17ed1f3b14d79802193ad2d0 [formerly 10e2a1cae0378163818ba94fbb54623224dad0f2]
    Former-commit-id: d6b0345171773331e2f2a806c20e2d387b1f6ad6

--- scripts/shell/final_run128HST/4_1_create_rim_dataset.sh
+++ scripts/shell/final_run128HST/4_1_create_rim_dataset.sh
@@ -16,26 +16,17 @@ python $CENSAI_PATH/scripts/create_rim_dataset_v2.py\
  $CENSAI_PATH/data/kappa128hst_VAE1_128hstfr_018_BN1_LS84_betaE0.6_betaDS10000_211016160612/\
  $CENSAI_PATH/data/kappa128hst_VAE1_128hstfr_000_BN0_LS84_betaE0.3_betaDS10000_211016160534/\
   --kappa_datasets_weights 0.4 0.4 0.2\
-  --cosmos_datasets $CENSAI_PATH/data/cosmos_23.5_finalrun_train $CENSAI_PATH/data/cosmos_23.5_preprocessed_highSNR_verydiffuse_train/\
-  --cosmos_datasets_weights 0.4 0.6\
+  --cosmos_datasets $CENSAI_PATH/data/cosmos_23.5_finalrun_train $CENSAI_PATH/data/cosmos_23.5_finalrun128_verydiffuse_train/\
+  --cosmos_datasets_weights 0.7 0.3\
   --compression_type=GZIP\
   --len_dataset=2000000\
   --lens_pixels=128\
-  --image_fov=7.68\
   --source_fov=3\
   --noise_rms=0.01\
-  --psf_sigma=0.06\
+  --psf_sigma=0.08\
   --buffer_size=10000\
   --batch_size=20\
   --tukey_alpha=0\
   --z_source=2.379\
   --z_lens=0.4457\
   --seed=42
-
-
-
-
-
-
-
-
