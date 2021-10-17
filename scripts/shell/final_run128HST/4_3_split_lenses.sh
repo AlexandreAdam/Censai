59b4b557284185bb44ad8917da1d94c701a5b740
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-17 15:07:35 -0400

    .
    Former-commit-id: 5bcd4c342810c6c227964c53a74965c5b4cb5cad
    Former-commit-id: 6c192095c6fa1a5e083892116392b3b6805028a1 [formerly 8cd62b19fa8a80ca731bc6e75630654a3d64c712]
    Former-commit-id: 2fc75ac60ca0959f0cd0a8fbc08dfe100ef426bb

--- scripts/shell/final_run128HST/4_3_split_lenses.sh
+++ scripts/shell/final_run128HST/4_3_split_lenses.sh
@@ -6,13 +6,12 @@
 #SBATCH --mem=32G			     # memory per node
 #SBATCH --time=0-04:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Validate_Lenses128hst
+#SBATCH --job-name=SplitLense
 # 00k-512_k128
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
-python $CENSAI_PATH/scripts/validate_lenses.py\
-  --dataset=$CENSAI_PATH/data/lenses128hst_TNG_VAE_2M\
-  --min_magnification=3\
-  --signal_threshold=0.1\
-  --example_per_worker=100000\
+python $CENSAI_PATH/scripts/split_lenses.py\
+  --dataset=$CENSAI_PATH/data/lenses128hst_TNG_VAE_2M_validated\
+  --split=0.9\
+  --example_per_shard=50000\
   --compression_type=GZIP
