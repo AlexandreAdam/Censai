a5f125effd5727026eb5e0fd13eff7a557a5a04b
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-20 00:11:53 -0400

    .

--- scripts/shell/final_run128HST/gridsearch/rim_shared_unet_control.sh
+++ scripts/shell/final_run128HST/gridsearch/rim_shared_unet_control.sh
@@ -10,54 +10,54 @@
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/experiments/rim_shared_unet_gridsearch.py\
-  --datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_2M_validated\
+  --datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_train\
+  --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_val\
   --compression_type=GZIP\
   --strategy=exhaustive\
-  --n_models=10\
+  --n_models=20\
   --forward_method=fft\
   --epochs=1000\
   --max_time=47\
   --optimizer ADAMAX\
-  --initial_learning_rate 1e-3\
+  --initial_learning_rate 1e-4\
   --decay_rate 0.9\
-  --decay_steps 5000\
+  --decay_steps 20000\
   --staircase\
   --clipping\
-  --patience=40\
+  --patience=80\
   --tolerance=0.01\
   --batch_size 10\
-  --train_split=0.95\
+  --train_split=0.90\
   --total_items 10000\
   --block_length=1\
-  --buffer_size=1000\
-  --steps 1 2 3 4 5 6 7 8 9 10\
+  --buffer_size=10000\
+  --steps 1 2 4 6 8 10 15 20\
   --time_weights quadratic\
   --adam 1\
   --upsampling_interpolation 0\
   --kappalog\
   --source_link lrelu4p\
-  --filters 32\
-  --filter_scaling 1\
+  --filters 16 32\
+  --filter_scaling 2\
   --kernel_size 3\
-  --layers 3\
+  --layers 4\
   --block_conv_layers 1\
   --kernel_size 3\
-  --resampling_kernel_size 1\
-  --input_kernel_size 1\
+  --resampling_kernel_size 3\
+  --input_kernel_size 11\
   --gru_kernel_size 3\
-  --activation relu\
-  --batch_norm 1\
-  --gru_architecture concat\
+  --activation leaky_relu\
+  --batch_norm 0\
+  --gru_architecture plus\
   --alpha 0.1\
   --source_init=1\
   --kappa_init=0.1\
   --cache_file=$SLURM_TMPDIR/cache\
-  --logdir=$CENSAI_PATH/logsSC2\
-  --logname_prefixe=RIMSU128_hTNG2nsvdO_UnrolledSteps2\
+  --logdir=$CENSAI_PATH/logsFR128hst\
+  --logname_prefixe=RIMSU128hst_control\
   --model_dir=$CENSAI_PATH/models\
   --checkpoints=5\
   --max_to_keep=1\
   --n_residuals=2\
   --seed 42\
-  --unroll_time_steps\
   --track_train
