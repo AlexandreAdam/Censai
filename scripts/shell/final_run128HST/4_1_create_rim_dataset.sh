96e6cdd759aad38281021ac431aa5f346d55a8b7
AlexandreAdam <adam.alexandre01123@gmail.com>
2021-10-20 10:15:16 -0400

    .

--- scripts/shell/final_run128HST/4_1_create_rim_dataset.sh
+++ scripts/shell/final_run128HST/4_1_create_rim_dataset.sh
@@ -3,7 +3,7 @@
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32G			     # memory per node
+#SBATCH --mem=64G			     # memory per node
 #SBATCH --time=1-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Create-RIM-Dataset-1
@@ -24,7 +24,7 @@ python $CENSAI_PATH/scripts/create_rim_dataset_v2.py\
   --source_fov=3\
   --noise_rms=0.01\
   --psf_sigma=0.06\
-  --buffer_size=50000\
+  --buffer_size=25000\
   --batch_size=20\
   --tukey_alpha=0\
   --block_length=1
