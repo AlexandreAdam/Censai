c3ba1c27957a61bf11717191d1d45a491070887b
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-23 11:58:03 -0400

    .

--- scripts/shell/final_run128HST/gridsearch/rim_shared_unetv2_control.sh
+++ scripts/shell/final_run128HST/gridsearch/rim_shared_unetv2_control.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-24
+#SBATCH --array=1-8
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
@@ -9,18 +9,19 @@
 #SBATCH --job-name=Train_RIM_SharedUnet_FR128hst
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
-python $CENSAI_PATH/scripts/train_rim_shared_unet.py\
-  --model_id=RIMSU128hst_control_010_TS10_F16_211020033949\
+python $CENSAI_PATH/scripts/experiments/rim_shared_unetv2_gridsearch.py\
   --datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_train\
   --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_val\
   --compression_type=GZIP\
+  --strategy=exhaustive\
+  --n_models=8\
   --forward_method=fft\
   --epochs=1000\
   --max_time=47\
   --optimizer ADAMAX\
-  --initial_learning_rate 5e-5\
+  --initial_learning_rate 1e-4\
   --decay_rate 0.9\
-  --decay_steps 60000\
+  --decay_steps 20000\
   --staircase\
   --clipping\
   --patience=80\
@@ -30,15 +31,34 @@ python $CENSAI_PATH/scripts/train_rim_shared_unet.py\
   --total_items 10000\
   --block_length=1\
   --buffer_size=10000\
+  --steps 4 8 10 15\
+  --time_weights quadratic\
+  --adam 1\
+  --upsampling_interpolation 0\
+  --kappalog\
+  --source_link lrelu4p\
+  --filters 16 32\
+  --filter_scaling 2\
+  --kernel_size 3\
+  --layers 4\
+  --block_conv_layers 1\
+  --kernel_size 3\
+  --resampling_kernel_size 3\
+  --input_kernel_size 11\
+  --gru_kernel_size 3\
+  --activation leaky_relu\
+  --batch_norm 0\
+  --gru_architecture plus\
+  --alpha 0.1\
+  --source_init=1\
+  --kappa_init=0.1\
   --cache_file=$SLURM_TMPDIR/cache\
   --logdir=$CENSAI_PATH/logsFR128hst\
+  --logname_prefixe=RIMSU128hst_control\
   --model_dir=$CENSAI_PATH/models\
   --checkpoints=5\
-  --max_to_keep=4\
+  --max_to_keep=1\
   --n_residuals=2\
   --seed 42\
   --track_train\
-  --reset_optimizer_states\
-  --json_override $CENSAI_PATH/models/RIMSU128hst_control_010_TS10_F16_211020033949/rim_hparams.json\
-  $CENSAI_PATH/models/RIMSU128hst_control_010_TS10_F16_211020033949/unet_hparams.json\
   --v2
