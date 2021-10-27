Fixed up rasterize for alpha variance and added Flux term in RIMv3
Former-commit-id: 448df96d22ddf05dc61747818ab2463c73bec56c
Former-commit-id: c1ee3f37b5b3b3d39d07de230e66652f743bbb20 [formerly 598fe7d98412adadcf2a58e9c2f2c7e52bbf1ecb]
Former-commit-id: d4cd8a37fa1ed666c4bfa820e8d147d88f73539d

censai/models/decoder.py
@@ -18,7 +18,8 @@ class Decoder(tf.keras.Model):
             dropout_rate=None,
             batch_norm=False,
             bilinear=False,
-            strides=2
+            strides=2,
+            output_activation=tf.nn.softplus
     ):
         super(Decoder, self).__init__()
         self._z_pix = z_reshape_pix
@@ -48,7 +49,7 @@ class Decoder(tf.keras.Model):
             filters=1,
             kernel_size=3,
             padding="SAME",
-            activation="linear",
+            activation=output_activation,
             kernel_regularizer=tf.keras.regularizers.l2(kernel_reg_amp),
             bias_regularizer=tf.keras.regularizers.l2(bias_reg_amp)
         )

censai/physical_modelv2.py
@@ -96,7 +96,7 @@ class PhysicalModelv2:
 
     def log_likelihood(self, source, kappa, y_true, noise_rms, psf):
         y_pred = self.forward(source, kappa, psf)
-        return 0.5 * tf.reduce_mean((y_pred - y_true) ** 2 / noise_rms ** 2, axis=(1, 2, 3))
+        return 0.5 * tf.reduce_mean((y_pred - y_true) ** 2 / noise_rms[:, None, None, None] ** 2, axis=(1, 2, 3))
 
     @staticmethod
     def lagrange_multiplier(y_true, y_pred):
@@ -132,7 +132,7 @@ class PhysicalModelv2:
         rho_sq = (beta1 - xs) ** 2 / (1 - es) + (beta2 - ys) ** 2 * (1 - es)
         lens = tf.math.exp(-0.5 * rho_sq / w ** 2)  # / 2 / np.pi / w**2
         psf_sigma = psf_sigma if psf_sigma is not None else self.image_fov / self.pixels
-        psf = self.psf_model(psf_sigma)
+        psf = self.psf_models(psf_sigma)
         lens = self.convolve_with_psf(lens, psf)
         return lens
 
@@ -145,7 +145,7 @@ class PhysicalModelv2:
         rho_sq = (beta1 - xs) ** 2 / (1 - es) + (beta2 - ys) ** 2 * (1 - es)
         lens = tf.math.exp(-0.5 * rho_sq / w ** 2)  # / 2 / np.pi / w**2
         psf_sigma = psf_sigma if psf_sigma is not None else self.image_fov / self.pixels
-        psf = self.psf_model(psf_sigma)
+        psf = self.psf_models(psf_sigma)
         lens = self.convolve_with_psf(lens, psf)
         return lens
 
@@ -261,8 +261,8 @@ class PhysicalModelv2:
         out[denominator != 0] = num[denominator != 0] / denominator[denominator != 0]
         return out
 
-    def psf_models(self, psf_sigma, cutout_size=16):
-        psf_sigma = np.atleast_1d(psf_sigma)[:, None, None, None]
+    def psf_models(self, psf_fwhm, cutout_size=16):
+        psf_sigma = np.atleast_1d(psf_fwhm[:, None, None, None]) / (2 * np.sqrt(2 * np.log(2)))
         r_squared = self.ximage**2 + self.yimage**2
         psf = tf.math.exp(-0.5 * r_squared / psf_sigma**2)
         psf = tf.image.crop_to_bounding_box(psf,

censai/rim_shared_unetv2.py
@@ -31,7 +31,7 @@ class RIMSharedUnetv2:
             epsilon=1e-8,
             kappa_init=1e-1,
             source_init=1e-3,
-            flux_prior_amp:float=1.
+            flux_lagrange_multiplier:float=1.
     ):
         self.physical_model = physical_model
         self.kappa_pixels = physical_model.kappa_pixels
@@ -47,7 +47,7 @@ class RIMSharedUnetv2:
         self.epsilon = epsilon
         self._kappa_init = kappa_init
         self._source_init = source_init
-        self.flux_prior_amp = flux_prior_amp
+        self.flux_lagrange_multiplier = flux_lagrange_multiplier
 
         if self.kappalog:
             if self.kappa_normalize:
@@ -119,8 +119,8 @@ class RIMSharedUnetv2:
         source, kappa, states = self.unet(source, kappa, source_grad, kappa_grad, states)
         return source, kappa, states
 
-    def __call__(self, lensed_image, outer_tape=nulltape):
-        return self.call(lensed_image, outer_tape)
+    def __call__(self, lensed_image, noise_rms, psf, outer_tape=nulltape):
+        return self.call(lensed_image, noise_rms, psf, outer_tape)
 
     def call(self, lensed_image, noise_rms, psf, outer_tape=nulltape):
         """
@@ -140,7 +140,7 @@ class RIMSharedUnetv2:
                     y_pred = self.physical_model.forward(self.source_link(source), self.kappa_link(kappa), psf)
                     flux_term = tf.square(tf.reduce_sum(y_pred, axis=(1, 2, 3)) - tf.reduce_sum(lensed_image, axis=(1, 2, 3)))
                     log_likelihood = 0.5 * tf.reduce_sum(tf.square(y_pred - lensed_image) / noise_rms[:, None, None, None]**2)
-                    cost = tf.reduce_mean(log_likelihood + self.flux_prior_amp * flux_term)
+                    cost = tf.reduce_mean(log_likelihood + self.flux_lagrange_multiplier * flux_term)
                 source_grad, kappa_grad = g.gradient(cost, [source, kappa])
                 source_grad, kappa_grad = self.grad_update(source_grad, kappa_grad, current_step)
             source, kappa, states = self.time_step(source, kappa, source_grad, kappa_grad, states)
@@ -149,7 +149,7 @@ class RIMSharedUnetv2:
             if current_step > 0:
                 chi_squared_series = chi_squared_series.write(index=current_step-1, value=log_likelihood)
         # last step score
-        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa))
+        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa), psf=psf, noise_rms=noise_rms)
         chi_squared_series = chi_squared_series.write(index=self.steps-1, value=log_likelihood)
         return source_series.stack(), kappa_series.stack(), chi_squared_series.stack()
 
@@ -181,7 +181,7 @@ class RIMSharedUnetv2:
             if current_step > 0:
                 chi_squared_series = chi_squared_series.write(index=current_step-1, value=log_likelihood)
         # last step score
-        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa))
+        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa), psf=psf, noise_rms=noise_rms)
         chi_squared_series = chi_squared_series.write(index=self.steps-1, value=log_likelihood)
         return source_series.stack(), kappa_series.stack(), chi_squared_series.stack()
 
@@ -211,11 +211,11 @@ class RIMSharedUnetv2:
             if current_step > 0:
                 chi_squared_series = chi_squared_series.write(index=current_step - 1, value=log_likelihood)
         # last step score
-        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa))
+        log_likelihood = self.physical_model.log_likelihood(y_true=lensed_image, source=self.source_link(source), kappa=self.kappa_link(kappa), psf=psf, noise_rms=noise_rms)
         chi_squared_series = chi_squared_series.write(index=self.steps - 1, value=log_likelihood)
         return source_series.stack(), kappa_series.stack(), chi_squared_series.stack()  # stack along 0-th dimension
 
-    def cost_function(self, lensed_image, source, kappa, outer_tape=nulltape, reduction=True):
+    def cost_function(self, lensed_image, source, kappa, noise_rms, psf, outer_tape=nulltape, reduction=True):
         """
 
         Args:
@@ -227,7 +227,7 @@ class RIMSharedUnetv2:
         Returns: The average loss over pixels, time steps and (if reduction=True) batch size.
 
         """
-        source_series, kappa_series, chi_squared = self.call(lensed_image, outer_tape=outer_tape)
+        source_series, kappa_series, chi_squared = self.call(lensed_image, psf=psf, noise_rms=noise_rms, outer_tape=outer_tape)
         source_cost = tf.reduce_sum(tf.square(source_series - self.source_inverse_link(source)), axis=0) / self.steps
         kappa_cost = tf.reduce_sum(tf.square(kappa_series - self.kappa_inverse_link(kappa)), axis=0) / self.steps
         chi = tf.reduce_sum(chi_squared, axis=0) / self.steps

scripts/create_rim_dataset_v2.py
@@ -65,8 +65,8 @@ def distributed_strategy(args):
 
     noise_a = (args.noise_rms_min - args.noise_rms_mean) / args.noise_rms_std
     noise_b = (args.noise_rms_max - args.noise_rms_mean) / args.noise_rms_std
-    psf_a = (args.psf_sigma_min - args.psf_sigma_mean) / args.psf_sigma_std
-    psf_b = (args.psf_sigma_max - args.psf_sigma_mean) / args.psf_sigma_std
+    psf_a = (args.psf_fwhm_min - args.psf_fwhm_mean) / args.psf_fwhm_std
+    psf_b = (args.psf_fwhm_max - args.psf_fwhm_mean) / args.psf_fwhm_std
 
     options = tf.io.TFRecordOptions(compression_type=args.compression_type)
     with tf.io.TFRecordWriter(os.path.join(args.output_dir, f"data_{THIS_WORKER}.tfrecords"), options) as writer:
@@ -78,7 +78,7 @@ def distributed_strategy(args):
                 break
             galaxies = window * galaxies
             noise_rms = truncnorm.rvs(noise_a, noise_b, loc=args.noise_rms_mean, scale=args.noise_rms_std, size=args.batch_size)
-            sigma = truncnorm.rvs(psf_a, psf_b, loc=args.psf_sigma_mean, scale=args.psf_sigma_std, size=args.batch_size)
+            sigma = truncnorm.rvs(psf_a, psf_b, loc=args.psf_fwhm_mean, scale=args.psf_fwhm_std, size=args.batch_size)
             psf = phys.psf_models(sigma, cutout_size=args.psf_cutout_size)
             lensed_images = phys.noisy_forward(galaxies, kappa, noise_rms=noise_rms, psf=psf)
 
@@ -120,10 +120,10 @@ if __name__ == '__main__':
     parser.add_argument("--noise_rms_mean",  default=0.01,      type=float,     help="Maximum white noise RMS added to lensed image")
     parser.add_argument("--noise_rms_std",   default=0.05,      type=float,     help="Maximum white noise RMS added to lensed image")
     parser.add_argument("--psf_cutout_size", required=True,     type=int,       help="Size of the cutout for the PSF (arceconds)")
-    parser.add_argument("--psf_sigma_min",   required=True,     type=float,     help="Minimum std of gaussian psf (arceconds)")
-    parser.add_argument("--psf_sigma_max",   required=True,     type=float,     help="Minimum std of gaussian psf (arceconds)")
-    parser.add_argument("--psf_sigma_mean",  required=True,     type=float,     help="Mean for the distribution of std of the gaussian psf (arceconds)")
-    parser.add_argument("--psf_sigma_std",   required=True,     type=float,     help="Std for distribution of stf of the gaussian psf (arceconds)")
+    parser.add_argument("--psf_fwhm_min",    required=True,     type=float,     help="Minimum std of gaussian psf (arceconds)")
+    parser.add_argument("--psf_fwhm_max",    required=True,     type=float,     help="Minimum std of gaussian psf (arceconds)")
+    parser.add_argument("--psf_fwhm_mean",   required=True,     type=float,     help="Mean for the distribution of std of the gaussian psf (arceconds)")
+    parser.add_argument("--psf_fwhm_std",    required=True,     type=float,     help="Std for distribution of stf of the gaussian psf (arceconds)")
 
     # Data generation params
     parser.add_argument("--buffer_size",    default=10000,       type=int,    help="buffer of shuffle, should be similar to number of examples per shard (at least greater than the largest shard)")

scripts/rasterize_halo_kappa_maps.py
@@ -162,7 +162,12 @@ def gaussian_kernel_rasterize(coords, mass, center, fov, dims=[0, 1], pixels=512
             variance += _np.sum((_mass * _np.exp(-0.5 * r_squared / _ell_hat ** 2) / (2 * _np.pi * _ell_hat ** 2))**2, axis=0)
             # Propagated uncertainty to deflection angles
             A = _np.exp(-0.5 * r_squared / _ell_hat ** 2)**2 - 2 * _np.exp(-0.5 * r_squared / _ell_hat ** 2)
-            alpha_variance += _np.sum((_mass[..., None] / _np.pi)**2 / (tf.maximum(r_squared[..., None]**2, 1e-16)) * (A[..., None] + 1) * xi[None, ...]**2)
+            _alpha_variance = tf.where(
+                condition=r_squared[..., None]**2 > 0,
+                x=(_mass[..., None] / _np.pi)**2 / r_squared[..., None]**2 * (A[..., None] + 1) * xi[None, ...]**2,
+                y=0.
+            )
+            alpha_variance += _np.sum(_alpha_variance)
     return Sigma, variance, alpha_variance
 
 