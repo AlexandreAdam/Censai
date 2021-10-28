4ba27227d674e7372ff74c054773ce5f8d1236fe
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-10-28 16:49:09 -0400

    .
    Former-commit-id: 10369431e28adb7fefea00d6dcd0a90f8bfc7135
    Former-commit-id: e4f4a32209e658888c94a1a5feb1e12e9f3f0f12 [formerly 70b2bdd3cb4eb681366e1719f23bc620d4c813f9]
    Former-commit-id: 073419fb3da2b4153b9a88eb33da7f941de48b53

--- scripts/shell/final_run128HST/4_1_create_rim_dataset_control_sie.sh
+++ scripts/shell/final_run128HST/4_1_create_rim_dataset_control_sie.sh
@@ -33,8 +33,8 @@ python $CENSAI_PATH/scripts/create_rim_analytical_kappa_maps.py\
   --noise_rms_max=0.1\
   --noise_rms_mean=0.01\
   --noise_rms_std=0.05\
-  --psf_cutout_size=20\
-  --psf_fwhm_min=0.08\
-  --psf_fwhm_max=0.5\
-  --psf_fwhm_mean=0.2\
-  --psf_fwhm_std=0.1
+  --psf_cutout_size=24\
+  --psf_sigma_min=0.06\
+  --psf_sigma_max=0.1\
+  --psf_sigma_mean=0.08\
+  --psf_sigma_std=0.01
