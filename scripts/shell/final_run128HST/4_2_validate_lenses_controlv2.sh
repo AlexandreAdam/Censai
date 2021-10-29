ab2a05a08ce982be9a9afca6e034e7f6743aaed7
Alexandre Adam <aadam@beluga3.int.ets1.calculquebec.ca>
2021-10-29 09:29:17 -0400

    params

---
+++ scripts/shell/final_run128HST/4_2_validate_lenses_controlv2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --array=1-20
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G			     # memory per node
+#SBATCH --time=0-04:00		# time (DD-HH:MM)
+#SBATCH --account=rrg-lplevass
+#SBATCH --job-name=Validate_Lenses128hst_v2
+# 00k-512_k128
+#SBATCH --output=%x-%j.out
+source $HOME/environments/censai3.8/bin/activate
+python $CENSAI_PATH/scripts/validate_lenses.py\
+  --dataset=$CENSAI_PATH/data/lenses128hst_TNG_rau_200k_control\
+  --min_magnification=3\
+  --signal_threshold=0.2\
+  --example_per_worker=10000\
+  --compression_type=GZIP
