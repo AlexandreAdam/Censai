Found the bug with the psf, also added fwhm in the data and finally changed clipping by value to the more theoritically grounded clip by norm in gradient descent with a clip value of 10.
Former-commit-id: b090c4c4a8519987a131decc2a53d42cd621c026
Former-commit-id: 9ee1eb050a38c2e89840bc58d80d7958b11302cf [formerly ad7b79915b00ae1e51de1d443549809c50917a41]
Former-commit-id: 82074208a9f6b193d34f2575572f7f38934f6376

scripts/create_rim_dataset_v2.py
@@ -78,8 +78,8 @@ def distributed_strategy(args):
                 break
             galaxies = window * galaxies
             noise_rms = truncnorm.rvs(noise_a, noise_b, loc=args.noise_rms_mean, scale=args.noise_rms_std, size=args.batch_size)
-            sigma = truncnorm.rvs(psf_a, psf_b, loc=args.psf_fwhm_mean, scale=args.psf_fwhm_std, size=args.batch_size)
-            psf = phys.psf_models(sigma, cutout_size=args.psf_cutout_size)
+            fwhm = truncnorm.rvs(psf_a, psf_b, loc=args.psf_fwhm_mean, scale=args.psf_fwhm_std, size=args.batch_size)
+            psf = phys.psf_models(fwhm, cutout_size=args.psf_cutout_size)
             lensed_images = phys.noisy_forward(galaxies, kappa, noise_rms=noise_rms, psf=psf)
             lensed_images = tf.nn.relu(lensed_images)
             lensed_images /= tf.reduce_max(lensed_images, axis=(1, 2, 3), keepdims=True)
@@ -94,7 +94,8 @@ def distributed_strategy(args):
                 kappa_fov=phys.kappa_fov,
                 source_fov=args.source_fov,
                 noise_rms=noise_rms,
-                psf=psf
+                psf=psf,
+                fwhm=fwhm
             )
             for record in records:
                 writer.write(record)