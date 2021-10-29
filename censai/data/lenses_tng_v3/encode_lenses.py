Found the bug with the psf, also added fwhm in the data and finally changed clipping by value to the more theoritically grounded clip by norm in gradient descent with a clip value of 10.

censai/data/lenses_tng_v3/encode_lenses.py
@@ -14,6 +14,7 @@ def encode_examples(
         source_fov: float,
         noise_rms: np.array,
         psf: tf.Tensor,
+        fwhm: np.array
 ):
     batch_size = galaxies.shape[0]
     source_pixels = galaxies.shape[1]
@@ -36,7 +37,8 @@ def encode_examples(
             "pixels": _int64_feature(pixels),
             "noise rms": _float_feature(noise_rms[j]),
             "psf": _bytes_feature(psf[j].numpy().tobytes()),
-            "psf pixels": _int64_feature(psf_pixels)
+            "psf pixels": _int64_feature(psf_pixels),
+            "fwhm": _float_feature(fwhm[j])
         }
         serialized_output = tf.train.Example(features=tf.train.Features(feature=features))
         record = serialized_output.SerializeToString()

censai/physical_modelv2.py
@@ -270,7 +270,7 @@ class PhysicalModelv2:
                                             offset_width=self.pixels//2 - cutout_size//2,
                                             target_width=cutout_size,
                                             target_height=cutout_size)
-        psf /= tf.reduce_sum(psf)
+        psf /= tf.reduce_sum(psf, axis=(1, 2, 3), keepdims=True)
         return psf
 
     def convolve_with_psf(self, images, psf):

scripts/train_rim_unet_wvae.py
@@ -285,8 +285,8 @@ def main(args):
         gradient1 = tape.gradient(cost, rim.source_model.trainable_variables)
         gradient2 = tape.gradient(cost, rim.kappa_model.trainable_variables)
         if args.clipping:
-            gradient1 = [tf.clip_by_value(grad, -10, 10) for grad in gradient1]
-            gradient2 = [tf.clip_by_value(grad, -10, 10) for grad in gradient2]
+            gradient1 = [tf.clip_by_norm(grad, 5.) for grad in gradient1]
+            gradient2 = [tf.clip_by_norm(grad, 5.) for grad in gradient2]
         optim.apply_gradients(zip(gradient1, rim.source_model.trainable_variables))
         optim.apply_gradients(zip(gradient2, rim.kappa_model.trainable_variables))
         chi_squared = tf.reduce_sum(chi_squared[-1]) / args.batch_size