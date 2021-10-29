Found the bug with the psf, also added fwhm in the data and finally changed clipping by value to the more theoritically grounded clip by norm in gradient descent with a clip value of 10.
Former-commit-id: f1cb92c0fa78da2de1b036f8a119844c71682ddf
Former-commit-id: 22d34fd3d25f06e442ef619218bfb655bb9fe9e2 [formerly d5f2c9ca05c0152a83e73622e56a6fb112cedf9b]
Former-commit-id: 6ee2c63860d9777998d7fb2ab790c23aea4ad66a

censai/data/lenses_tng_v3/decode_lenses.py
@@ -20,7 +20,8 @@ def decode_all(record_bytes):
               'pixels': tf.io.FixedLenFeature([], tf.int64),
               'noise rms': tf.io.FixedLenFeature([], tf.float32),
               "psf": tf.io.FixedLenFeature([], tf.string),
-              "psf pixels":  tf.io.FixedLenFeature([], tf.int64)
+              "psf pixels":  tf.io.FixedLenFeature([], tf.int64),
+              "fwhm": tf.io.FixedLenFeature([], tf.float32)
           })
     kappa = tf.io.decode_raw(example['kappa'], tf.float32)
     source = tf.io.decode_raw(example['source'], tf.float32)

scripts/train_rim_shared_unetv2.py
@@ -286,7 +286,7 @@ def main(args):
             cost = tf.reduce_sum(kappa_cost + source_cost) / args.batch_size
         gradient = tape.gradient(cost, rim.unet.trainable_variables)
         if args.clipping:
-            gradient = [tf.clip_by_value(grad, -10, 10) for grad in gradient]
+            gradient = [tf.clip_by_norm(grad, 10.) for grad in gradient]
         optim.apply_gradients(zip(gradient, rim.unet.trainable_variables))
         # Update metrics with "converged" score
         chi_squared = tf.reduce_sum(chi_squared[-1]) / args.batch_size