20f2842e5d7cd219273ee1ff8ee60c5ecf9c44af
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-11-01 10:36:44 -0400

    Changed v3 params
    Former-commit-id: 48132c90a72967872944a6747d1626a6cc12b216
    Former-commit-id: 68d98876922a9ae3d088203be7565e9fe66bfd42 [formerly 939da8a2aa2bed4897c75422da55d3902323f5c5]
    Former-commit-id: a02959564c5f34d783e911c1b9165b7986e07bce

--- scripts/shell/final_run128HST/gridsearch/rim_shared_unetv3_control.sh
+++ scripts/shell/final_run128HST/gridsearch/rim_shared_unetv3_control.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-8
+#SBATCH --array=1-48
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
@@ -14,30 +14,33 @@ python $CENSAI_PATH/scripts/experiments/rim_shared_unetv3_gridsearch.py\
   --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_rau_200k_control_validated_val\
   --compression_type=GZIP\
   --strategy=exhaustive\
-  --n_models=8\
+  --n_models=48\
   --forward_method=fft\
-  --epochs=1000\
+  --epochs=200\
   --max_time=47\
   --optimizer ADAMAX\
-  --initial_learning_rate 1e-4\
-  --decay_rate 0.9\
-  --decay_steps 20000\
+  --initial_learning_rate 1e-3\
+  --decay_rate 0.5\
+  --decay_steps 50000\
   --staircase\
   --clipping\
   --patience=80\
   --tolerance=0.01\
-  --batch_size 10\
+  --batch_size 1\
   --train_split=0.90\
   --total_items 10000\
   --block_length=1\
   --buffer_size=10000\
-  --steps 4 8 10 12\
-  --time_weights quadratic\
+  --steps 5 8\
+  --flux_lagrange_multiplier 1.\
+  --time_weights uniform quadratic\
+  --kappa_residual_weights uniform linear\
+  --source_residual_weights uniform linear sqrt\
   --adam 1\
   --upsampling_interpolation 0\
   --kappalog\
-  --source_link relu\
-  --filters 16 32\
+  --source_link lrelu4p\
+  --filters 32\
   --filter_scaling 2\
   --kernel_size 3\
   --layers 4\
@@ -46,11 +49,11 @@ python $CENSAI_PATH/scripts/experiments/rim_shared_unetv3_gridsearch.py\
   --resampling_kernel_size 3\
   --input_kernel_size 11\
   --gru_kernel_size 3\
-  --activation leaky_relu\
-  --batch_norm 1\
+  --activation relu\
+  --batch_norm 0\
   --gru_architecture plus\
   --alpha 0.1\
-  --source_init=1\
+  --source_init=0\
   --kappa_init=0.1\
   --cache_file=$SLURM_TMPDIR/cache\
   --logdir=$CENSAI_PATH/logsFR128hst\
@@ -61,4 +64,3 @@ python $CENSAI_PATH/scripts/experiments/rim_shared_unetv3_gridsearch.py\
   --n_residuals=2\
   --seed 42\
   --track_train\
-  --v2
