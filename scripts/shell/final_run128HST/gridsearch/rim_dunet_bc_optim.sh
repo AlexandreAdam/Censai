169fb85f3aab30ac16c5bc72982aa368b4d098c7
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-11-04 16:59:08 -0400

    .
    Former-commit-id: 7ae15f2d1ef001c729d0d75acbaf984078726b7b
    Former-commit-id: 24a5b3975ac74239b08a6be02bc0a56acdfbba89 [formerly ed625c215093d5732477f6606aaab28e288fbab4]
    Former-commit-id: 305288da3dbca6e526d8227911bb57a2fc6018f9

--- scripts/shell/final_run128HST/gridsearch/rim_dunet_bc_optim.sh
+++ scripts/shell/final_run128HST/gridsearch/rim_dunet_bc_optim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-3
+#SBATCH --array=1-8
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
@@ -10,11 +10,11 @@
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/experiments/rim_unet_blockcoord_gridsearch.py\
-  --datasets $CENSAI_PATH/data/lenses128hst_TNG_rau_200k_control_validated_train\
-  --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_rau_200k_control_validated_val\
+  --datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_train\
+  --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated_val\
   --compression_type=GZIP\
   --strategy=exhaustive\
-  --n_models=3\
+  --n_models=8\
   --forward_method=fft\
   --epochs=100000\
   --max_time=46\
@@ -29,14 +29,14 @@ python $CENSAI_PATH/scripts/experiments/rim_unet_blockcoord_gridsearch.py\
   --train_split=1\
   --total_items 10000\
   --block_length=1\
-  --steps 2 5 10\
+  --steps 4 8\
   --time_weights quadratic\
   --adam 1\
   --kappalog\
   --kappa_init=1e-1\
   --source_init=1\
   --delay 0\
-  --source_link lrelu4p\
+  --source_link identity\
   --kappa_filters 16\
   --kappa_filter_scaling 2\
   --kappa_kernel_size 3\
