688d65b132f7d7973b6cf3bdf3e4e75221b7a587
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-11-15 10:08:59 -0500

    Fixed draw batch when we don't generate randomly. Fixed default redshifts and kappa parameters

--- scripts/shell/final_run128HST/1_1_cosmos_to_tfrecords_distributed.sh
+++ scripts/shell/final_run128HST/1_1_cosmos_to_tfrecords_distributed.sh
@@ -13,6 +13,6 @@ python $CENSAI_PATH/scripts/cosmos_to_tfrecords_distributed.py\
   --pixels=158\
   --sample=23.5\
   --exclusion_level=marginal\
-  --min_flux=50\
+  --min_flux=1\
   --cosmos_dir=$HOME/projects/rrg-lplevass/data/COSMOS/COSMOS_23.5_training_sample/\
   --output_dir=$HOME/scratch/Censai/data/cosmos_23.5_finalrun158/
