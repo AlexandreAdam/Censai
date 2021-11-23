b835197180c28270c0e01041391a81daf237a62b
AlexandreAdam <adam.alexandre01123@gmail.com>
2021-11-23 10:27:44 -0500

    .
    Former-commit-id: 3d70b661bb09daf4176f0cd90a0c72d0b9c9a1d4
    Former-commit-id: bc6f2beabe9254ab75c12c583751fbdf8a16eb79 [formerly e64d3518b57bbc14593b5b7263853cf782aa9480]
    Former-commit-id: 75b87db42a11c40e5a800945167c1f0e9d9583f3

--- scripts/shell/final_run128HST/gridsearch/continue_rim_shared_unetv4.sh
+++ scripts/shell/final_run128HST/gridsearch/continue_rim_shared_unetv4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --array=1-16
+#SBATCH --array=1-8
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
 #SBATCH --time=2-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Train_RIM_SharedUnetv4_FR128hst
+#SBATCH --job-name=Continue_Train_RIM_SharedUnetv4_FR128hst
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/experiments/continue_rim_shared_unetv4.py\
