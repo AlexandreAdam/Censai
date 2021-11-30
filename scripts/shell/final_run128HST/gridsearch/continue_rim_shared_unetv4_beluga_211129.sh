bd06baf64d37075e63b8849aaed66401a4d24d64
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-11-29 20:15:30 -0500

    .

--- scripts/shell/final_run128HST/gridsearch/continue_rim_shared_unetv4_beluga_211129.sh
+++ scripts/shell/final_run128HST/gridsearch/continue_rim_shared_unetv4_beluga_211129.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --array=1-8
+#SBATCH --array=1-3
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=2-00:00		# time (DD-HH:MM)
+#SBATCH --time=4-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Continue_Train_RIM_SharedUnetv4_FR128hst
 #SBATCH --output=%x-%j.out
@@ -14,10 +14,12 @@ python $CENSAI_PATH/scripts/experiments/continue_rim_shared_unetv4.py\
   --val_datasets $CENSAI_PATH/data/lenses128hst_TNG_VAE_2M_validated_val $CENSAI_PATH/data/lenses128hst_SIE_200k_control_validated_val\
   --compression_type=GZIP\
   --models \
-
+ RIMSU128hstv4_augmented_005_RMSP0_TS8_F32_TWuniform_KRWsqrt_211124133122\
+ RIMSU128hstv4_augmented_007_RMSP0_TS8_F32_TWquadratic_KRWsqrt_21112413422\
+ RIMSU128hstv4_augmented_011_RMSP0_TS10_F16_TWquadratic_KRWsqrt_21112414050\
   --forward_method=fft\
   --epochs=200\
-  --max_time=47\
+  --max_time=95\
   --optimizer ADAMAX\
   --initial_learning_rate 1e-5\
   --decay_rate=0.9\
@@ -37,3 +39,4 @@ python $CENSAI_PATH/scripts/experiments/continue_rim_shared_unetv4.py\
   --n_residuals=2\
   --seed=15\
   --track_train\
+  --use_residual_weights
