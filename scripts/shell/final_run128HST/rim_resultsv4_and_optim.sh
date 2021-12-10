15479d48dfaaf09ee910efacbf0a69be1f2aabb7
Alexandre Adam <adam.alexandre01213@gmail.com>
2021-12-09 20:39:52 -0500

    .
    Former-commit-id: aae2a0c0e7dda5fc4046c2fde683f76f1b0d01b5
    Former-commit-id: 90f700b0c18491bdfaeb45df890c4d29a1ea6307 [formerly 8e0411ce75765f0dd05036bb4786a437315bd2f1]
    Former-commit-id: e593c892bf4007af90ed72e3b682828b6f53241e

--- scripts/shell/final_run128HST/rim_resultsv4_and_optim.sh
+++ scripts/shell/final_run128HST/rim_resultsv4_and_optim.sh
@@ -10,8 +10,8 @@
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/rim_results_v4_and_optim.py\
-  --model_prefix=RIMSU128hstv4\
-  --source_model=RIMSource128hstv3_control_001_A0_L2_FLM0.0_211108220845\
+  --model=RIMSU128hstv4_augmented_003_K3_L5_BCL2_211124140837_continue_lr6e-05_211129202839\
+  --source_model=RIMSource128hstv3_control_009_A1_L3_FLM0.0_211108220845\
   --val_dataset=lenses128hst_TNG_rau_200k_control_denoised_validated_val\
   --train_dataset=lenses128hst_TNG_rau_200k_control_denoised_validated_train\
   --test_dataset=lenses128hst_TNG_rau_200k_control_denoised_testset_validated\
