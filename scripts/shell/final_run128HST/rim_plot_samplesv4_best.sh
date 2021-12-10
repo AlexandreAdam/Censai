d088ceee80cc92aa1e1d0eadd447cc0624642ec0
Alexandre Adam <aadam@narval1.narval.calcul.quebec>
2021-12-09 20:40:11 -0500

    .
    Former-commit-id: aec3fed2db5e77a0ba9a1e2cfd6637d073af6136
    Former-commit-id: ba458e5622701e08dce27e0419dd4227dee1c344 [formerly d1fb0a056a08e34a2b9c4ebdf1f047762fbb0cda]
    Former-commit-id: 69a9daaf042b7f5e5b99ecc102d4576ccf06e792

--- scripts/shell/final_run128HST/rim_plot_samplesv4_best.sh
+++ scripts/shell/final_run128HST/rim_plot_samplesv4_best.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#SBATCH --array=1
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G			     # memory per node
-#SBATCH --time=1-0:00		# time (DD-HH:MM)
+#SBATCH --time=0-02:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
-#SBATCH --job-name=Save_RIM_resultsv4
+#SBATCH --job-name=Plot_RIM_resultsv4
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
-python $CENSAI_PATH/scripts/rim_results_v4.py\
-  --model_list RIMSU128hstv4_augmented_003_K3_L5_BCL2_211124140837_continue_lr6e-05_211129202839\
-  --source_model=RIMSource128hstv3_control_001_A0_L2_FLM0.0_211108220845\
+python $CENSAI_PATH/scripts/rim_plot_samples_v4.py\
+  --model_prefix=RIMSU128hstv4_augmented_003_K3_L5_BCL2_211124140837_continue_lr6e-05_211129202839\
+  --minimum_date=211124000000\
+  --source_model=RIMSource128hstv3_control_003_A0_L3_FLM0.0_211108220845\
   --val_dataset=lenses128hst_TNG_rau_200k_control_denoised_validated_val\
   --train_dataset=lenses128hst_TNG_rau_200k_control_denoised_validated_train\
   --test_dataset=lenses128hst_TNG_rau_200k_control_denoised_testset_validated\
-  --sie_size=300\
-  --train_size=300\
-  --val_size=300\
-  --test_size=2000\
-  --buffer_size=1000\
+  --train_size=0\
+  --val_size=0\
+  --test_size=400\
+  --buffer_size=400\
   --batch_size=10\
-  --lens_coherence_bins=40\
-  --source_coherence_bins=40\
-  --kappa_coherence_bins=40
