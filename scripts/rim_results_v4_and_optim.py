2dd280bfac7eeb4c8cf24d7ad4dbdee1e3b766ff
AlexandreAdam <adam.alexandre01123@gmail.com>
2021-12-19 13:49:25 -0500

    .

--- scripts/rim_results_v4_and_optim.py
+++ scripts/rim_results_v4_and_optim.py
@@ -154,7 +154,7 @@ def distributed_strategy(args):
             g.create_dataset(name="chi_squared_reoptimized_series", shape=[data_len, args.re_optimize_steps], dtype=np.float32)
             g.create_dataset(name="source_optim_mse", shape=[data_len], dtype=np.float32)
             g.create_dataset(name="kappa_optim_mse_series", shape=[data_len, args.re_optimize_steps], dtype=np.float32)
-            g.create_dataset(name="source_optim_mse", shape=[data_len], dtype=np.float32)
+            g.create_dataset(name="kappa_optim_mse", shape=[data_len], dtype=np.float32)
             g.create_dataset(name="kappa_optim_mse_series", shape=[data_len, args.re_optimize_steps], dtype=np.float32)
             g.create_dataset(name="lens_coherence_spectrum", shape=[data_len, args.lens_coherence_bins], dtype=np.float32)
             g.create_dataset(name="source_coherence_spectrum",  shape=[data_len, args.source_coherence_bins], dtype=np.float32)
