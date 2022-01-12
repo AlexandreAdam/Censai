24f7b7b432ebd1f6382a7a65fb191ca41636858e
Alexandre Adam <adam.alexandre01213@gmail.com>
2022-01-11 20:15:58 -0500

    got correct dataset
    Former-commit-id: cb22ad5da5923a83bd6aae0680ef7cb831f5ed94
    Former-commit-id: 77d855b031d8ae05fed8b870b968fe0e27aa58c4 [formerly 1b38aadf283d0c3680f563ed428b892e9f9ad5b6]
    Former-commit-id: 250da452ffbc1ecc3dde7a47c11d94f43b7ee92c

---
+++ scripts/shell/final_run128HST/knn_ranking_local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python $CENSAI_PATH/scripts/knn_vae.py\
+  --dataset=$CENSAI_PATH2/data/lenses128hst_TNG_VAE_200k_control_validated\
+  --kappa_vae=$CENSAI_PATH2/models/VAE1_128hstfr_002_LS16_dr0.7_betaE0.2_betaDS5000_211115153537\
+  --source_dataset=$CENSAI_PATH2/data/cosmosFR_VAE1_COSMOSFR_003_F32_NLleaky_relu_LS32_betaE0.1_betaDS100000_211018104400\
+  --output_name=k50_vae_ranking_220111\
+  -k=50\
+  --sample_size=20
