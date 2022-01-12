cb22ad5da5923a83bd6aae0680ef7cb831f5ed94
Alexandre Adam <adam.alexandre01213@gmail.com>
2022-01-11 20:15:58 -0500

    got correct dataset

--- scripts/shell/final_run128HST/knn_ranking.sh
+++ scripts/shell/final_run128HST/knn_ranking.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=%x-%j.out
 source $HOME/environments/censai3.8/bin/activate
 python $CENSAI_PATH/scripts/knn_vae.py\
-  --dataset=$CENSAI_PATH/data/lenses128hst_TNG_VAE_200k_control_validated\
+  --dataset=$CENSAI_PATH/data/lenses128hst_TNG_rau_200k_control\
   --kappa_vae=$CENSAI_PATH/models/VAE1_128hstfr_002_LS16_dr0.7_betaE0.2_betaDS5000_211115153537\
   --source_dataset=$CENSAI_PATH/data/cosmosFR_VAE1_COSMOSFR_003_F32_NLleaky_relu_LS32_betaE0.1_betaDS100000_211018104400\
   --output_name=k50_vae_ranking_220111\
