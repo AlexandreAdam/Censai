9a0290ecba783584cb14686e9903a94a1feb36fb
AlexandreAdam <adam.alexandre01123@gmail.com>
2022-01-16 00:28:43 -0500

    Changed reot script for experiment 4: Add noise to observation durint reopt + 10 examples for MSE.

--- scripts/rim_results_v4_and_optim_posterior.py
+++ scripts/rim_results_v4_and_optim_posterior.py
@@ -181,12 +181,14 @@ def distributed_strategy(args):
                     with tf.GradientTape() as tape:
                         tape.watch(unet.trainable_variables)
                         # log likelihood
+                        noise = tf.random.normal(shape=lens.shape, stddev=noise_rms)
+                        s, k, chi_sq = rim.call(lens - noise, noise_rms, psf, outer_tape=tape) # experiment 4, try to add noise to input to remove noise leakage?
                         # s, k, chi_sq = rim.call(lens, noise_rms, psf, outer_tape=tape) # was using this in experiment 1 and 2 + only single sample for mse
-                        # cost = tf.reduce_mean(chi_sq)
+                        cost = tf.reduce_mean(chi_sq)
                         # log prior
                         s, k, chi_sq = rim.call(sampled_lens, noise_rms, tf.tile(psf, [args.sample_size, 1, 1, 1]), outer_tape=tape)
                         _kappa_mse = tf.reduce_sum(wk(10**sampled_kappa) * (k - sampled_kappa) ** 2, axis=(2, 3, 4))
-                        cost = tf.reduce_mean(_kappa_mse)
+                        cost += tf.reduce_mean(_kappa_mse)
                         cost += tf.reduce_mean((s - sampled_source)**2)
                         cost += tf.reduce_sum(rim.unet.losses)  # weight decay
 
