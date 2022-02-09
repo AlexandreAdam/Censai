95328933affae65d471c52ad346b54f5c0dbe681
Alexandre Adam <aadam@narval2.narval.calcul.quebec>
2022-02-09 18:00:54 -0500

    .
    Former-commit-id: caa452df865e11da27068b16c5db45103f29e35b
    Former-commit-id: 8565c8e1c7f78b9c44487bce0e49d3ef8a616ab4 [formerly ce0bbe50fc14c7a299ec7c3dcea2dac7606a70f0]
    Former-commit-id: 2f0b2500ec0ef06bbdfeef9fbb3235d8dd2f913f

--- scripts/shell/final_run128HST/rim_resultsv4_and_optim_posterior_on_vae.sh
+++ scripts/shell/final_run128HST/rim_resultsv4_and_optim_posterior_on_vae.sh
@@ -14,7 +14,7 @@ python $CENSAI_PATH/scripts/rim_results_v4_and_optim_posterior_on_vae.py\
   --model=RIMSU128hstv4_augmented_003_K3_L5_BCL2_211124140837_continue_lr6e-05_211129202839\
   --source_vae=VAE1_COSMOSFR_001_F16_NLleaky_relu_LS32_betaE0.1_betaDS100000_220112114306\
   --kappa_vae=VAE1_128hstfr_002_LS16_dr0.7_betaE0.2_betaDS5000_211115153537\
-  --size=1000\
+  --size=2000\
   --sample_size=2\
   --re_optimize_steps=1000\
   --observation_coherence_bins=40\
