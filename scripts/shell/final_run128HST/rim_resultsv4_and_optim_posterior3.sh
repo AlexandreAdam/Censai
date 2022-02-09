372d988d193a111294cef2caaff8c28196659b41
Alexandre Adam <adam.alexandre01213@gmail.com>
2022-02-09 17:59:26 -0500

    .

--- scripts/shell/final_run128HST/rim_resultsv4_and_optim_posterior3.sh
+++ scripts/shell/final_run128HST/rim_resultsv4_and_optim_posterior3.sh
@@ -14,11 +14,10 @@ python $CENSAI_PATH/scripts/rim_results_v4_and_optim_posterior3.py\
   --model=RIMSU128hstv4_augmented_003_K3_L5_BCL2_211124140837_continue_lr6e-05_211129202839\
   --source_vae=VAE1_COSMOSFR_001_F16_NLleaky_relu_LS32_betaE0.1_betaDS100000_220112114306\
   --kappa_vae=VAE1_128hstfr_002_LS16_dr0.7_betaE0.2_betaDS5000_211115153537\
-  --test_dataset=lenses128hst_TNG_rau_200k_control_denoised_testset_validated\
   --size=3000\
   --sample_size=2\
   --buffer_size=1000\
-  --lens_coherence_bins=40\
+  --observation_coherence_bins=40\
   --source_coherence_bins=40\
   --kappa_coherence_bins=40\
   --seed=42\
